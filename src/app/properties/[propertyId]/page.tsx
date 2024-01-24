a7d6eb93ef6f5f869f5321bef0311cd8bb8331eb
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-24 22:27:37 +0000

    Itegrate Convex; query db; add property render condition; make fn async

--- src/app/properties/[propertyId]/page.tsx
+++ src/app/properties/[propertyId]/page.tsx
@@ -1,7 +1,21 @@
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "@/../convex/_generated/api";
+
 type PropertyPageParams = {
   params: { propertyId: string };
 };
 
-export default function PropertyPage({ params }: PropertyPageParams) {
-  return <h1>Property Page</h1>;
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default async function PropertyPage({ params }: PropertyPageParams) {
+  const property = await convex.query(api.properties.queries.getProperty, {
+    name: params.propertyId,
+  });
+
+  return !property ? null : (
+    <div>
+      <h1>Property Page</h1>
+      <p key={property.propertyId}>{property.propertyDetails.name}</p>
+    </div>
+  );
 }
