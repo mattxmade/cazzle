19b073e807321bbf200ce1a744cc022123af3783
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-24 18:23:47 +0000

    Create dynamic route; create Page Component; initial commit

---
+++ src/app/estate-agents/[agentId]/page.tsx
@@ -0,0 +1,9 @@
+type EstateAgentProfilePageParams = {
+  params: { agentId: string };
+};
+
+const EstateAgentProfilePageProps = (params: EstateAgentProfilePageParams) => (
+  <h1>Estate Agent Page</h1>
+);
+
+export default EstateAgentProfilePageProps;
