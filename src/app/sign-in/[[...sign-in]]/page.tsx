995c4f4d64762fbea42b249672d24914d26ff864
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-26 23:13:40 +0000

    Create sign-up page route: add Clerk SignIn Component + Material UI layout

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,44 @@
+import { SignIn } from "@clerk/nextjs";
+
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+import theme from "@/theme";
+
+const backgroundColor = theme.palette?.mode
+  ? theme.palette.mode === "light"
+    ? theme.palette.grey[50]
+    : theme.palette.grey[900]
+  : "";
+
+export default function SignInPage() {
+  return (
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundRepeat: "no-repeat",
+          backgroundColor: backgroundColor,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            marginTop: "25%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <SignIn />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
