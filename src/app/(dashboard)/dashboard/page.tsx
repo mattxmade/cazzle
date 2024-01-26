81ec457629d60ff9a9f42cebc3e98e623fdc4466
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-26 01:42:27 +0000

    Add Clerk Component flows; add redirect on sign-out; make route dynamic

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,18 @@
+import { redirect } from "next/navigation";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+
 import Dashboard from "./_components/Dashboard";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardPage() {
-  return <Dashboard />;
+  return (
+    <>
+      <SignedIn>
+        <Dashboard />
+      </SignedIn>
+
+      <SignedOut>{redirect("/")}</SignedOut>
+    </>
+  );
 }
