39d591a37ad8dcca4dc2b4a7775eb383a7ce710c
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-28 22:54:06 +0000

    Declare types; consume ariaLabel prop

--- src/components/ui/InfoButton.tsx
+++ src/components/ui/InfoButton.tsx
@@ -1,8 +1,17 @@
-import { Button } from "@mui/material";
+"use client";
+
+import Button from "@mui/material/Button";
 import InfoIcon from "@mui/icons-material/InfoOutlined";
 
-const InfoButton = () => (
-  <Button sx={{ minWidth: 0, padding: 0, borderRadius: "100%" }}>
+type InfoButtonProps = {
+  ariaLabel: string;
+};
+
+const InfoButton = ({ ariaLabel }: InfoButtonProps) => (
+  <Button
+    sx={{ minWidth: 0, padding: 0, borderRadius: "100%" }}
+    aria-label={ariaLabel}
+  >
     <InfoIcon fontSize="small" sx={{ color: "grey" }} />
   </Button>
 );
