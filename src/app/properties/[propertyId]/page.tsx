6e91936422e8926c4d95dddd498d9eb9e84d0b1c
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-28 00:43:12 +0000

    Create initial layout using Material Grid

--- src/app/properties/[propertyId]/page.tsx
+++ src/app/properties/[propertyId]/page.tsx
@@ -1,6 +1,16 @@
 import { fetchQuery } from "convex/nextjs";
 import { api } from "@/../convex/_generated/api";
 
+import Image from "next/image";
+import { Box, Button } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+
+import HouseSharpIcon from "@mui/icons-material/HouseSharp";
+import TuneSharpIcon from "@mui/icons-material/TuneSharp";
+
+// import theme from "@/theme";
+// TODO theme.spacing
+
 type PropertyPageParams = {
   params: { propertyId: string };
 };
@@ -13,9 +23,32 @@ export default async function PropertyPage({ params }: PropertyPageParams) {
   });
 
   return !property ? null : (
-    <div>
-      <h1>Property Page</h1>
-      <p key={property.property_id}>{property.name}</p>
-    </div>
+    <Box component="main" sx={{ display: "grid", padding: "4rem 2rem" }}>
+      <Button sx={{ width: "100%", padding: "0 2rem" }}>
+        <Image src="" alt="" height={120} style={{ width: "100%" }} />
+      </Button>
+
+      <Grid container spacing={1} width="100%">
+        <Grid xs={8}>
+          <Button sx={{ width: "100%" }}>
+            <Image src="" alt="" height={360} style={{ width: "100%" }} />
+          </Button>
+        </Grid>
+        <Grid xs={4}>
+          <TuneSharpIcon sx={{ width: "100%" }} />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} width="100%">
+        <Grid xs={8}>
+          <HouseSharpIcon
+            sx={{ width: "100%", backgroundColor: "lightgrey" }}
+          />
+        </Grid>
+        <Grid xs={4}>
+          <TuneSharpIcon sx={{ width: "100%" }} />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
