00c80f11d3632e54c067dafd85d73668f43e1d9a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-28 02:12:26 +0000

    Update IconBar Component: Create Grid layout

--- src/components/ui/IconBar.tsx
+++ src/components/ui/IconBar.tsx
@@ -1,3 +1,21 @@
+import { Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+
 export default function IconBar() {
-  return <></>;
+  return (
+    <Grid container spacing={2} width="100%">
+      <Grid xs={3} flexGrow={1}>
+        <Typography variant="body2">TEXT 1</Typography>
+      </Grid>
+      <Grid xs={3} flexGrow={1}>
+        <Typography variant="body2">TEXT 2</Typography>
+      </Grid>
+      <Grid xs={3} flexGrow={1}>
+        <Typography variant="body2">TEXT 3</Typography>
+      </Grid>
+      <Grid xs={3} flexGrow={1}>
+        <Typography variant="body2">TEXT 4</Typography>
+      </Grid>
+    </Grid>
+  );
 }
