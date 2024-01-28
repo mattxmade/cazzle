5ee2eb7da2778aaa59ec23cf91fe944744ec9cc3
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-28 01:09:59 +0000

    Create property details header layout; provision property values

--- src/app/properties/[propertyId]/page.tsx
+++ src/app/properties/[propertyId]/page.tsx
@@ -2,13 +2,15 @@ import { fetchQuery } from "convex/nextjs";
 import { api } from "@/../convex/_generated/api";
 
 import Image from "next/image";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 import HouseSharpIcon from "@mui/icons-material/HouseSharp";
 import TuneSharpIcon from "@mui/icons-material/TuneSharp";
+import CalculateIcon from "@mui/icons-material/CalculateOutlined";
+import InfoIcon from "@mui/icons-material/InfoOutlined";
 
-// import theme from "@/theme";
+import theme from "@/theme";
 // TODO theme.spacing
 
 type PropertyPageParams = {
@@ -40,12 +42,28 @@ export default async function PropertyPage({ params }: PropertyPageParams) {
       </Grid>
 
       <Grid container spacing={2} width="100%">
+        {/* Property Details */}
         <Grid xs={8}>
           <HouseSharpIcon
             sx={{ width: "100%", backgroundColor: "lightgrey" }}
           />
+
+          <Typography variant="body2">{property.displayAddress}</Typography>
+
+          <Box sx={{ gap: "0.5rem", display: "flex", alignItems: "center" }}>
+            <Typography variant="h4">{property.fullMarketPrice} </Typography>
+            <Button sx={{ minWidth: 0, padding: 0, borderRadius: "100%" }}>
+              <InfoIcon fontSize="small" />
+            </Button>
+          </Box>
+
+          <Button sx={{ gap: "0.5rem", display: "flex", alignItems: "center" }}>
+            <CalculateIcon fontSize="medium" />
+            <Typography variant="body2">Monthly mortgage payments</Typography>
+          </Button>
         </Grid>
         <Grid xs={4}>
+          {/* Property Aside */}
           <TuneSharpIcon sx={{ width: "100%" }} />
         </Grid>
       </Grid>
