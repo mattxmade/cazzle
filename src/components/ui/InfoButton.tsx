ceaa872a00884e8d8075df6c751a636046aa0207
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-28 23:10:40 +0000

    Provision Component states; assign button event callbacks

--- src/components/ui/InfoButton.tsx
+++ src/components/ui/InfoButton.tsx
@@ -1,19 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Button from "@mui/material/Button";
 import InfoIcon from "@mui/icons-material/InfoOutlined";
 
 type InfoButtonProps = {
   ariaLabel: string;
+  children?: React.ReactNode;
 };
 
-const InfoButton = ({ ariaLabel }: InfoButtonProps) => (
-  <Button
-    sx={{ minWidth: 0, padding: 0, borderRadius: "100%" }}
-    aria-label={ariaLabel}
-  >
-    <InfoIcon fontSize="small" sx={{ color: "grey" }} />
-  </Button>
-);
+const InfoButton = ({ ariaLabel }: InfoButtonProps) => {
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  const handleEvent = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  return (
+    <>
+      <Button
+        sx={{ minWidth: 0, padding: 0, borderRadius: "100%" }}
+        aria-label={ariaLabel}
+        onClick={handleEvent}
+        onMouseOver={handleEvent}
+        onMouseOut={handleEvent}
+      >
+        <InfoIcon fontSize="small" sx={{ color: "grey" }} />
+      </Button>
+    </>
+  );
+};
 
 export default InfoButton;
