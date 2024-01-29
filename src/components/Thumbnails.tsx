793ac089566d28dbfd75021aa282843598d3c391
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-29 17:10:48 +0000

    Create Thumbnails Components: Declare types; provision elements; initial commit

---
+++ src/components/Thumbnails.tsx
@@ -0,0 +1,7 @@
+type ThumbnailsProps = {
+  thumbnails: { src: string; alt: string; type: string }[];
+};
+
+const Thumbnails = ({ thumbnails }: ThumbnailsProps) => <>Thumbnails</>;
+
+export default Thumbnails;
