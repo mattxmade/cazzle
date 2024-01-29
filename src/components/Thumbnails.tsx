f477c7473e0537200057da05f2d3312ad130234d
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-29 17:26:18 +0000

    Add Mui Components; setup ImageList; map thumbnails data

--- src/components/Thumbnails.tsx
+++ src/components/Thumbnails.tsx
@@ -1,7 +1,21 @@
+import Link from "@mui/material/Link";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
 type ThumbnailsProps = {
   thumbnails: { src: string; alt: string; type: string }[];
 };
 
-const Thumbnails = ({ thumbnails }: ThumbnailsProps) => <>Thumbnails</>;
+const Thumbnails = ({ thumbnails }: ThumbnailsProps) => (
+  <ImageList>
+    {thumbnails.map((item) => (
+      <ImageListItem key={item.src}>
+        <Link>
+          <img srcSet={item.src} alt={item.alt} loading="lazy" />
+        </Link>
+      </ImageListItem>
+    ))}
+  </ImageList>
+);
 
 export default Thumbnails;
