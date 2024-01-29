150d8ec6e7d3fdc76698cab7a232252b4112a5a0
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-29 21:08:53 +0000

    Complete Thumbnails layout

--- src/components/Thumbnails.tsx
+++ src/components/Thumbnails.tsx
@@ -3,18 +3,48 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
 type ThumbnailsProps = {
+  additional: number | null;
   thumbnails: { src: string; alt: string; type: string }[];
 };
 
-const Thumbnails = ({ thumbnails }: ThumbnailsProps) => (
-  <ImageList>
+const ImageListItemStyle = {
+  width: 75,
+  height: 75,
+  display: "grid",
+  overflow: "hidden",
+  borderRadius: "0.2rem",
+  backgroundColor: "lavender",
+  ":hover": { cursor: "pointer" },
+};
+
+const Thumbnails = ({ additional, thumbnails }: ThumbnailsProps) => (
+  <ImageList
+    sx={{ gap: "0.8rem !important", display: "flex", flexWrap: "wrap" }}
+  >
     {thumbnails.map((item) => (
-      <ImageListItem key={item.src}>
-        <Link>
-          <img srcSet={item.src} alt={item.alt} loading="lazy" />
+      <ImageListItem key={item.src} sx={ImageListItemStyle}>
+        <Link width={75} height={75} display="grid" justifyContent="center">
+          <img
+            srcSet={`${item.src}?h=75&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.src}?h=75&fit=crop&auto=format`}
+            alt={item.alt}
+            loading="lazy"
+          />
         </Link>
       </ImageListItem>
     ))}
+
+    {additional ? (
+      <ImageListItem sx={ImageListItemStyle}>
+        <Link
+          alignSelf="center"
+          justifySelf="center"
+          sx={{ fontSize: "1.4rem", textDecoration: "none" }}
+        >
+          +{additional}
+        </Link>
+      </ImageListItem>
+    ) : null}
   </ImageList>
 );
 
