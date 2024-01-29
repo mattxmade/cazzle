c5bb95e9c58a6888aac5c278ae4cb4b0d5c6ae09
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-29 15:19:19 +0000

    Add Mui Icon

--- src/components/ui/buttons/LikeButton.tsx
+++ src/components/ui/buttons/LikeButton.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import Button from "@mui/material/Button";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 
-const LikeButton = () => <Button>Like Button</Button>;
+const LikeButton = () => (
+  <Button>
+    <FavoriteBorderOutlinedIcon />
+  </Button>
+);
 
 export default LikeButton;
