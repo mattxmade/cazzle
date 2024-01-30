42dfa28797dc620b776fdf458608446339e2f953
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-30 21:27:13 +0000

    Create Card ImageList from mapped mediaData; add sx styles + layout

--- src/components/layout/MediaGallery.tsx
+++ src/components/layout/MediaGallery.tsx
@@ -1,9 +1,43 @@
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
 type MediaGalleryProps = {
   mediaData: { src: string; alt: string; type: string }[];
 };
 
 const MediaGallery = ({ mediaData }: MediaGalleryProps) => {
-  return <h1>Media Gallery</h1>;
+  return (
+    <Card elevation={3} sx={{ width: "fit-content" }}>
+      <ImageList
+        gap={12}
+        cols={2}
+        rowHeight={120}
+        sx={{ width: "fit-content" }}
+      >
+        {mediaData.map((item) => (
+          <ImageListItem
+            key={item.src}
+            sx={{
+              width: 160,
+              height: "auto",
+              backgroundColor: "inherit",
+            }}
+          >
+            <Button>
+              <img
+                srcSet={`${item.src}?w=180&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.src}?w=180&fit=crop&auto=format`}
+                alt={item.alt}
+                loading="lazy"
+              />
+            </Button>
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Card>
+  );
 };
 
 export default MediaGallery;
