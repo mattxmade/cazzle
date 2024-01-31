139043d79130d6afc8ac64afbcebd1a0f6d21718
Matt <97047080+mattxmade@users.noreply.github.com>
2024-01-31 18:47:57 +0000

    Implement Mui Component layout; add types

--- src/components/layout/MediaCarousel.tsx
+++ src/components/layout/MediaCarousel.tsx
@@ -1,14 +1,55 @@
 "use client";
 
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
-type MediaCarouselProps = {
-  showControls?: boolean;
-  mediaData: { src: string; alt: string; type: string }[];
-};
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-const MediaCarousel = ({ mediaData, showControls }: MediaCarouselProps) => {
-  return <Container>Media Carousel</Container>;
+import { MediaData } from "@/types";
+
+type MediaCarouselProps = {
+  item: number;
+  maxWidth?: number;
+  showControls?: boolean;
+  children?: React.ReactNode;
+  handleNavigation: (item: number) => void;
+} & MediaData;
+
+const MediaCarousel = (props: MediaCarouselProps) => {
+  return (
+    <>
+      <Button>
+        <ArrowBackIosIcon />
+      </Button>
+      <Container sx={{ position: "relative", overflow: "hidden" }}>
+        {props.mediaData.map((item, i) => (
+          <Box
+            key={"carousel_" + item.src}
+            sx={{
+              display: i > 0 ? "none" : "grid",
+            }}
+          >
+            <img
+              key={item.src}
+              src={item.src}
+              alt={item.alt}
+              loading="lazy"
+              width={"100%"}
+            />
+            <Typography>
+              {props.item} of {props.mediaData.length}
+            </Typography>
+          </Box>
+        ))}
+      </Container>
+      <Button>
+        <ArrowForwardIosIcon />
+      </Button>
+    </>
+  );
 };
 
 export default MediaCarousel;
