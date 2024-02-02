307fdc227798c9891d10335a7859a5bb776b810b
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 11:47:32 +0000

    Add Basic Mui App Bar

--- src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
@@ -1,3 +1,27 @@
-export default function DashboardAppBar() {
-  return <h1>DashboardAppBar</h1>;
-}
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const DashboardAppBar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="dashboard menu"
+            sx={{ marginRight: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default DashboardAppBar;
