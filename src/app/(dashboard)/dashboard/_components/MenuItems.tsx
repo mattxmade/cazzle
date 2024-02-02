a9301025faeb45ce128a037182088cdd57f2acb7
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 22:36:42 +0000

    Add Mui List Components; declare types; compose initial layout

--- src/app/(dashboard)/dashboard/_components/MenuItems.tsx
+++ src/app/(dashboard)/dashboard/_components/MenuItems.tsx
@@ -1,5 +1,25 @@
-const MenuItems = () => {
-  return <h2>Menu Items</h2>;
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+
+type MenuItemsProps = {
+  menuItems: { name: string; text: string; icon: string }[];
+  handleMenuItem: (name: string) => void;
+};
+
+const MenuItems = ({ menuItems, handleMenuItem }: MenuItemsProps) => {
+  return (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon></ListItemIcon>
+          <ListItemText primary={"Menu Item"} />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
 };
 
 export default MenuItems;
