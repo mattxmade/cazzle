fac2b492235fbe7ca45b9f4aec3b34d90ad37c5f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 11:27:47 +0000

    Create dashboard route page; initial commit

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,3 @@
-import { redirect } from "next/navigation";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-
-import Dashboard from "./_components/Dashboard";
-
-export const dynamic = "force-dynamic";
-
-export default async function DashboardPage() {
-  return (
-    <>
-      <SignedIn>
-        <Dashboard />
-      </SignedIn>
-
-      <SignedOut>{redirect("/")}</SignedOut>
-    </>
-  );
+export default function DashboardPage() {
+  return <h1>Dashboard Page</h1>;
 }
