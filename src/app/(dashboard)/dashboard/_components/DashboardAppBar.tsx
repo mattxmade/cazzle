81dc34898c988c658203862820e408c00c5b0e22
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 13:39:46 +0000

    Add Mui AppBar min variant; handle open state; add drawerWidth props; Add client directive

--- src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-import AppBar from "@mui/material/AppBar";
+import MuiAppBar, {
+  type AppBarProps as MuiAppBarProps,
+} from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -8,14 +12,19 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
+import { styled } from "@mui/material/styles";
+import { useState } from "react";
+
 type DashboardAppBarProps = {
   title: string;
 };
 
 const DashboardAppBar = ({ title }: DashboardAppBarProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" open={open} drawerWidth={240}>
         <Toolbar>
           <IconButton
             size="large"
@@ -23,6 +32,7 @@ const DashboardAppBar = ({ title }: DashboardAppBarProps) => {
             color="inherit"
             aria-label="dashboard menu"
             sx={{ marginRight: 2 }}
+            onClick={() => setOpen(!open)}
           >
             <MenuIcon />
           </IconButton>
@@ -46,3 +56,27 @@ const DashboardAppBar = ({ title }: DashboardAppBarProps) => {
 };
 
 export default DashboardAppBar;
+
+type AppBarProps = {
+  drawerWidth: number;
+  open?: boolean;
+} & MuiAppBarProps;
+
+// https://mui.com/material-ui/react-drawer/#mini-variant-drawer
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open, drawerWidth }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
