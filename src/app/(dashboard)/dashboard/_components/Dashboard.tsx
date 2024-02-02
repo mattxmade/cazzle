ea32f267791880dcdfb8c9fcc8828307ff5d0584
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 13:58:53 +0000

    Create handler funcs; Add Components; configire props; add client directive

--- src/app/(dashboard)/dashboard/_components/Dashboard.tsx
+++ src/app/(dashboard)/dashboard/_components/Dashboard.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import Container from "@mui/material/Container";
+
+import DashboardAppBar from "./DashboardAppBar";
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Container disableGutters maxWidth={false}>
+      <DashboardAppBar
+        title="Dashboard"
+        open={open}
+        handleDrawerOpen={handleDrawerOpen}
+      />
+    </Container>
+  );
 };
 
 export default Dashboard;
