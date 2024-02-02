56e9e8debb0f5882b2437bbad1d9ca1cc12dc46f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-02 11:56:59 +0000

    Declare types; Add Mui Typography Cmpnt; consume props

--- src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
@@ -3,8 +3,13 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
 
-const DashboardAppBar = () => {
+type DashboardAppBarProps = {
+  title: string;
+};
+
+const DashboardAppBar = ({ title }: DashboardAppBarProps) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,6 +23,9 @@ const DashboardAppBar = () => {
           >
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            {title}
+          </Typography>
         </Toolbar>
       </AppBar>
     </Box>
