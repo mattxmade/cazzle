80c6c7766cb156c9d3b1913e7655df4e2f109afd
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-03 15:36:50 +0000

    Complete NavBar layout; map items; provision auth ui

--- src/components/core/NavBar.tsx
+++ src/components/core/NavBar.tsx
@@ -1,9 +1,49 @@
+import Link from "next/link";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignInButton,
+} from "@clerk/nextjs";
+
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 type NavBarProps = {
-  items: { name: string; slug: string }[];
+  navItems: { name: string; slug: string }[];
 };
 
-const NavBar = ({ items }: NavBarProps) => {
-  return <h2>Nav Bar</h2>;
+const NavBar = ({ navItems }: NavBarProps) => {
+  return (
+    <Stack component="nav" direction={"row"} spacing={7} alignItems="center">
+      <Stack direction={"row"} spacing={3}>
+        {navItems.map((item) => (
+          <Link href={item.slug} style={{ textDecoration: "none" }}>
+            <Typography variant="button">{item.name}</Typography>
+          </Link>
+        ))}
+      </Stack>
+
+      <Stack spacing={1}>
+        <SignedIn>
+          <SignOutButton>
+            <Button>
+              <Typography variant="button">Sign Out</Typography>
+            </Button>
+          </SignOutButton>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>
+            <Button>
+              <Typography variant="button">Sign In</Typography>
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default NavBar;
