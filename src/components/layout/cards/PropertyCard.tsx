43566f78823aad9fc00a8a181fe941515e3fa7f8
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-03 21:34:47 +0000

    Complete initial layout: Add Mui Card family elements; add card content

--- src/components/layout/cards/PropertyCard.tsx
+++ src/components/layout/cards/PropertyCard.tsx
@@ -1,5 +1,35 @@
-const PropertyCard = () => {
-  return <h2>Property Card</h2>;
+import Card from "@mui/material/Card/Card";
+import CardHeader from "@mui/material/CardHeader/CardHeader";
+import CardContent from "@mui/material/CardContent/CardContent";
+import CardMedia from "@mui/material/CardMedia/CardMedia";
+
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import { PropertyListing } from "@/types";
+
+type PropertyCardProps = {
+  propertyData: PropertyListing;
+  children?: React.ReactNode;
+};
+
+const PropertyCard = ({ propertyData, children }: PropertyCardProps) => {
+  return (
+    <Container maxWidth="xs">
+      <Card elevation={2}>
+        <CardHeader
+          avatar
+          title={
+            <Typography variant="h2" fontSize={22} fontWeight={400}>
+              {propertyData.name}
+            </Typography>
+          }
+        />
+        <CardMedia>{children}</CardMedia>
+        <CardContent></CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default PropertyCard;
