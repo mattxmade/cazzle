d33314681886c9198cbdacb2cf6299317d7c87a5
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-03 15:18:21 +0000

    Complete Header layout; Add NavBar Cmpnt; consume props

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,5 +1,40 @@
-const Header = () => {
-  return <h1>Header</h1>;
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import NavBar from "./NavBar";
+
+type HeaderProps = {
+  heading?: string;
+  subheading?: string;
+  logo?: string;
+};
+
+const Header = ({ heading, subheading, logo }: HeaderProps) => {
+  return (
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{ backgroundColor: "lightcyan" }}
+    >
+      <Container
+        component="header"
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          paddingTop: 2,
+          paddingBottom: 2,
+          justifyContent: "space-between",
+        }}
+      >
+        <Stack>
+          <Typography variant="h3">{heading}</Typography>
+        </Stack>
+
+        <NavBar />
+      </Container>
+    </Container>
+  );
 };
 
 export default Header;
