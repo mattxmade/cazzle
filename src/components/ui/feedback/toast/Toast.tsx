74905d368e530e01a4c6f01e5bd25083c2b5f2cc
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-04 22:34:43 +0000

    Implement Snackbar Transition; consume props; add render condition

--- src/components/ui/feedback/toast/Toast.tsx
+++ src/components/ui/feedback/toast/Toast.tsx
@@ -1,17 +1,35 @@
 import Snackbar from "@mui/material/Snackbar/Snackbar";
 import SnackbarContent from "@mui/material/SnackbarContent/SnackbarContent";
+import Slide, { SlideProps } from "@mui/material/Slide";
+
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction="up" />;
+}
 
 type ToastProps = {
+  open: boolean;
   message: string;
-  action: React.ReactNode;
-  children?: typeof SnackbarContent;
+  action?: React.ReactNode;
+  handleClose?: () => void;
+  children?: React.ReactNode;
 };
 
-const Toast = ({ message }: ToastProps) => {
+const Toast = (props: ToastProps) => {
   return (
-    <Snackbar>
-      <SnackbarContent message={message} />
+    <Snackbar
+      open={props.open}
+      key={SlideTransition.name}
+      onClose={props.handleClose}
+      autoHideDuration={2200}
+      TransitionComponent={SlideTransition}
+    >
+      {!props.children ? (
+        <SnackbarContent message={props.message} />
+      ) : (
+        <>{props.children}</>
+      )}
     </Snackbar>
   );
 };
+
 export default Toast;
