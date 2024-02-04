301ee2d74b00f2987016b9f57d0a254f020f8fd0
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-04 21:27:13 +0000

    Add Mui Snackbar Components; assign props; declare types

--- src/components/ui/feedback/Toast.tsx
+++ src/components/ui/feedback/Toast.tsx
@@ -1,4 +1,17 @@
-const Toast = () => {
-  return <h3>Toast</h3>;
+import Snackbar from "@mui/material/Snackbar/Snackbar";
+import SnackbarContent from "@mui/material/SnackbarContent/SnackbarContent";
+
+type ToastProps = {
+  message: string;
+  action: React.ReactNode;
+  children?: typeof SnackbarContent;
+};
+
+const Toast = ({ message }: ToastProps) => {
+  return (
+    <Snackbar>
+      <SnackbarContent message={message} />
+    </Snackbar>
+  );
 };
 export default Toast;
