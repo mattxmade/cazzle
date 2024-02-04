a0c7adc01aef05072b3b0127e57d76908b519221
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-04 13:38:28 +0000

    Add Grid breakpoints; progress layout; add content

--- src/components/layout/cards/PropertyCard.tsx
+++ src/components/layout/cards/PropertyCard.tsx
@@ -3,16 +3,21 @@ import CardHeader from "@mui/material/CardHeader/CardHeader";
 import CardContent from "@mui/material/CardContent/CardContent";
 import CardMedia from "@mui/material/CardMedia/CardMedia";
 
-import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import Stack from "@mui/material/Stack";
-import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
 import HotelOutlinedIcon from "@mui/icons-material/HotelOutlined";
 import ShowerOutlinedIcon from "@mui/icons-material/ShowerOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 import { PropertyListing } from "@/types";
+import Link from "next/link";
+import CardIcons from "./CardIcons";
+import React from "react";
 
 type PropertyCardProps = {
   propertyData: PropertyListing;
@@ -20,12 +25,29 @@ type PropertyCardProps = {
 };
 
 const PropertyCard = ({ propertyData, children }: PropertyCardProps) => {
+  const LinkProps = {
+    href: `/properties/${propertyData.slug}`,
+    style: {
+      color: "black",
+      textDecoration: "none",
+    } as React.CSSProperties,
+  };
+
   const css: React.CSSProperties = {
     color: "ivory",
   };
 
   return (
-    <Container component="article" maxWidth="xs">
+    <Grid
+      xs={12}
+      sm={12}
+      md={5.8}
+      lg={3.8}
+      xl={2.8}
+      component="article"
+      margin={0}
+      sx={{ position: "relative" }}
+    >
       <Card elevation={2}>
         <CardMedia>{children}</CardMedia>
         <CardHeader
@@ -66,7 +88,7 @@ const PropertyCard = ({ propertyData, children }: PropertyCardProps) => {
           }}
         />
 
-        <CardContent>
+        <CardContent sx={{ paddingBottom: "8px !important" }}>
           <Stack
             direction="row"
             flexWrap="wrap"
@@ -96,9 +118,59 @@ const PropertyCard = ({ propertyData, children }: PropertyCardProps) => {
               </Card>
             </Stack>
           </Stack>
+
+          <Divider sx={{ marginTop: 2, marginBottom: 1 }} />
+
+          <Link {...LinkProps}>
+            <Typography variant="subtitle2">{propertyData.summary}</Typography>
+          </Link>
+
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ marginTop: 1 }}
+          >
+            <Card
+              sx={{
+                width: "fit-content",
+                padding: 0.5,
+                backgroundColor: "darkslategrey",
+              }}
+            >
+              <Link href={LinkProps.href}>
+                <CardIcons
+                  color={css.color}
+                  imageIndex={1}
+                  videoCount={0}
+                  imageCount={propertyData.galleryImages.length}
+                  floorplanCount={propertyData.floorplanImages.length}
+                />
+              </Link>
+            </Card>
+
+            <Stack direction="row" alignItems="center">
+              <Button
+                aria-label="favourite property"
+                sx={{
+                  minWidth: 0,
+                  minHeight: 0,
+                  padding: 0.75,
+                  borderRadius: "100%",
+                }}
+              >
+                <Card sx={{ lineHeight: 0, padding: 1, borderRadius: "100%" }}>
+                  <FavoriteBorderOutlinedIcon />
+                </Card>
+              </Button>
+              <Typography variant="body1" fontWeight={500}>
+                Save
+              </Typography>
+            </Stack>
+          </Stack>
         </CardContent>
       </Card>
-    </Container>
+    </Grid>
   );
 };
 
