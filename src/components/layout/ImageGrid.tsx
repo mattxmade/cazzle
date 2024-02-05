541eebc713398a0c9cc3c759aaea7578ea0417a9
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 14:39:23 +0000

    Implement Mui useMediaQueryHook; add breakpoint render condition

--- src/components/layout/ImageGrid.tsx
+++ src/components/layout/ImageGrid.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import Button from "@mui/material/Button";
 
 import Image from "next/image";
 import NextLink from "next/link";
 
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 type ImageGrid = {
   imageData: { src: string; alt: string }[];
   link: string;
 };
 
 const ImageGrid = ({ imageData, link }: ImageGrid) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Grid container sx={{ height: 435 }}>
       <Grid sm={12} md={8} height="inherit" paddingRight={"4px"}>
@@ -32,35 +39,38 @@ const ImageGrid = ({ imageData, link }: ImageGrid) => {
           />
         </Button>
       </Grid>
-      <Grid
-        container
-        xs={12}
-        sm={12}
-        md={4}
-        height="inherit"
-        paddingLeft={"4px"}
-      >
-        {[imageData[1], imageData[2]].map((item, i) => (
-          <Button
-            fullWidth
-            key={"image_" + item.alt + "_" + i}
-            aria-label={"image " + i + 1}
-            sx={{
-              height: "calc(50% - 4px)",
-              overflow: "hidden",
-              position: "relative",
-              alignSelf: i === 0 ? "start" : "end",
-            }}
-          >
-            <Image
-              fill
-              src={item.src}
-              alt={item.alt}
-              style={{ objectFit: "cover" }}
-            />
-          </Button>
-        ))}
-      </Grid>
+
+      {matches ? null : (
+        <Grid
+          container
+          xs={12}
+          sm={12}
+          md={4}
+          height="inherit"
+          paddingLeft={"4px"}
+        >
+          {[imageData[1], imageData[2]].map((item, i) => (
+            <Button
+              fullWidth
+              key={"image_" + item.alt + "_" + i}
+              aria-label={"image " + i + 1}
+              sx={{
+                height: "calc(50% - 4px)",
+                overflow: "hidden",
+                position: "relative",
+                alignSelf: i === 0 ? "start" : "end",
+              }}
+            >
+              <Image
+                fill
+                src={item.src}
+                alt={item.alt}
+                style={{ objectFit: "cover" }}
+              />
+            </Button>
+          ))}
+        </Grid>
+      )}
     </Grid>
   );
 };
