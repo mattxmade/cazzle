a87e57358336b9542f8ccdef49da8c377a662389
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 13:59:51 +0000

    Implement Mui Grid; setup layout

--- src/components/layout/ImageGrid.tsx
+++ src/components/layout/ImageGrid.tsx
@@ -1,5 +1,10 @@
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+
 const ImageGrid = () => {
-  <h2>Image Grid</h2>;
+  <Grid container sx={{ height: 435 }}>
+    <Grid sm={12} md={8} paddingRight={"4px"}></Grid>
+    <Grid container sm={0} md={4} paddingLeft={"4px"}></Grid>
+  </Grid>;
 };
 
 export default ImageGrid;
