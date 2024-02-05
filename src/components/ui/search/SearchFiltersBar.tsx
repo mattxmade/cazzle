bf9a856b9fdf63e9461426b688079dd33eb0360a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 00:14:22 +0000

    Add form filter select input; create state + handler func

--- src/components/ui/search/SearchFiltersBar.tsx
+++ src/components/ui/search/SearchFiltersBar.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import InputLabel from "@mui/material/InputLabel";
+import { useState } from "react";
+
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import { content } from "@/app/content";
+
 const SearchFiltersBar = () => {
-  return <h2>Search Filters Bar</h2>;
+  const [location, setLocation] = useState("");
+
+  const handleLocationChange = (e: SelectChangeEvent) => {
+    const select = e.target as HTMLSelectElement;
+    setLocation(select.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 110 }}>
+      <InputLabel>Location</InputLabel>
+      <Select
+        value={location}
+        onChange={handleLocationChange}
+        autoWidth
+        label="Location"
+      >
+        <MenuItem aria-label="None" value=""></MenuItem>
+        {content.search.filters.locations.map((location) => (
+          <MenuItem value={location.value}>{location.text}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 };
 
 export default SearchFiltersBar;
