222902fd24a7faf8a79f5487a8acf9bb4412b157
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 16:43:39 +0000

    Add Mui Grid + breakpoints; declare types; destructure props

--- src/app/(properties)/_components/ListingMain.tsx
+++ src/app/(properties)/_components/ListingMain.tsx
@@ -1,5 +1,13 @@
-const ListingMain = () => {
-  <h1>Lisitng Main Section</h1>;
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+
+import { ListingData } from "@/types";
+
+type ListingMain = {
+  listing: ListingData;
+};
+
+const ListingMain = ({ listing }: ListingMain) => {
+  return <Grid container xs={12} sm={12} md={8}></Grid>;
 };
 
 export default ListingMain;
