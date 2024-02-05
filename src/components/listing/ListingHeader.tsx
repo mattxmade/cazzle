c1d372dbea2271da768a14bdc8ae1f42cde2e7a8
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 18:01:56 +0000

    Update ListingHeader Component: Add elments + contents; assign props

--- src/components/listing/ListingHeader.tsx
+++ src/components/listing/ListingHeader.tsx
@@ -1,5 +1,33 @@
-const ListingHeader = () => {
-  return <h1>Listing Header</h1>;
+import Stack from "@mui/material/Stack/Stack";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import LikeButton from "@/components/ui/buttons/LikeButton";
+import ShareButton from "@/components/ui/buttons/ShareButton";
+
+type ListingHeaderProps = {
+  heading: string;
+  children?: React.ReactNode;
+};
+
+const ListingHeader = ({ heading, children }: ListingHeaderProps) => {
+  return (
+    <Stack
+      width={"100%"}
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Typography variant="h1" fontSize={"1rem"} fontWeight={"500"}>
+        {heading}
+      </Typography>
+
+      <Stack direction="row">
+        <ShareButton />
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <LikeButton />
+      </Stack>
+    </Stack>
+  );
 };
 
 export default ListingHeader;
