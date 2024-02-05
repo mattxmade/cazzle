be373f44d323342aab02ba808c21e0d32da6c3de
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 14:19:30 +0000

    Add grid content; add grid breakpoints; amend types

--- src/components/layout/ImageGrid.tsx
+++ src/components/layout/ImageGrid.tsx
@@ -1,15 +1,68 @@
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Button from "@mui/material/Button";
+
+import Image from "next/image";
+import NextLink from "next/link";
 
 type ImageGrid = {
-  imageData: { src: string; alt: string };
-  link?: string;
+  imageData: { src: string; alt: string }[];
+  link: string;
 };
 
 const ImageGrid = ({ imageData, link }: ImageGrid) => {
-  <Grid container sx={{ height: 435 }}>
-    <Grid sm={12} md={8} paddingRight={"4px"}></Grid>
-    <Grid container sm={0} md={4} paddingLeft={"4px"}></Grid>
-  </Grid>;
+  return (
+    <Grid container sx={{ height: 435 }}>
+      <Grid sm={12} md={8} height="inherit" paddingRight={"4px"}>
+        <Button
+          fullWidth
+          component={NextLink}
+          href={`${link + 0}`}
+          aria-label="image 1"
+          sx={{
+            height: "inherit",
+            overflow: "hidden",
+          }}
+        >
+          <Image
+            fill
+            priority
+            src={imageData[0].src}
+            alt={imageData[0].alt}
+            style={{ objectFit: "cover" }}
+          />
+        </Button>
+      </Grid>
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        md={4}
+        height="inherit"
+        paddingLeft={"4px"}
+      >
+        {[imageData[1], imageData[2]].map((item, i) => (
+          <Button
+            fullWidth
+            key={"image_" + item.alt + "_" + i}
+            aria-label={"image " + i + 1}
+            sx={{
+              height: "calc(50% - 4px)",
+              overflow: "hidden",
+              position: "relative",
+              alignSelf: i === 0 ? "start" : "end",
+            }}
+          >
+            <Image
+              fill
+              src={item.src}
+              alt={item.alt}
+              style={{ objectFit: "cover" }}
+            />
+          </Button>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ImageGrid;
