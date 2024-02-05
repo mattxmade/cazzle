a9506d0e100a8e2593d714a75f9e545ccf7d11c1
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 02:19:24 +0000

    Complete search filter form: Add select input content; tweak layout

--- src/components/ui/search/SearchFiltersBar.tsx
+++ src/components/ui/search/SearchFiltersBar.tsx
@@ -26,49 +26,115 @@ const SearchFiltersBar = () => {
     const select = e.target as HTMLSelectElement;
 
     if (!validFilterInput(type, select.value)) return;
+    setFilterInputs((prev) => ({ ...prev, [type]: select.value }));
   };
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 110 }}>
-      <InputLabel>Location</InputLabel>
-      <Select
-        value={filterInputs.location}
-        onChange={(e) => handleInputChange(e, "location")}
-        autoWidth
-        label="Location"
-      >
-        <MenuItem aria-label="None" value=""></MenuItem>
-        {content.search.filters.locations.map((location) => (
-          <MenuItem value={location.value}>{location.text}</MenuItem>
-        ))}
-      </Select>
+    <Stack
+      component="form"
+      direction="row"
+      alignItems="center"
+      justifyContent="space-around"
+    >
+      <FormControl sx={{ m: 1, minWidth: 125 }}>
+        <InputLabel>Location</InputLabel>
+        <Select
+          value={filterInputs.location}
+          onChange={(e) => handleInputChange(e, "location")}
+          autoWidth
+          label="Location"
+        >
+          <MenuItem aria-label="None" value=""></MenuItem>
+          {content.search.filters.locations.map((location) => (
+            <MenuItem key={"location_" + location.value} value={location.value}>
+              {location.text}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
 
       <Stack direction="row" spacing={2}>
-        <InputLabel>Min Price</InputLabel>
-        <Select
-          value={filterInputs.minPrice}
-          onChange={(e) => handleInputChange(e, "minPrice")}
-          autoWidth
-          label="Min Price"
-        >
-          {content.search.filters.locations.map((location) => (
-            <MenuItem value={location.value}>{location.text}</MenuItem>
-          ))}
-        </Select>
+        <FormControl sx={{ m: 1, minWidth: 110 }}>
+          <InputLabel>MinPrice</InputLabel>
+          <Select
+            value={filterInputs.minPrice}
+            onChange={(e) => handleInputChange(e, "minPrice")}
+            autoWidth
+            label="MinPrice"
+          >
+            {content.search.filters.prices.map((price) => (
+              <MenuItem key={"minPrice_" + price.value} value={price.value}>
+                {price.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-        <InputLabel>Max Price</InputLabel>
+        <FormControl sx={{ m: 1, minWidth: 110 }}>
+          <InputLabel>MaxPrice</InputLabel>
+          <Select
+            value={filterInputs.maxPrice}
+            onChange={(e) => handleInputChange(e, "maxPrice")}
+            autoWidth
+            label="MaxPrice"
+          >
+            {content.search.filters.prices.map((price) => (
+              <MenuItem key={"maxPrice_" + price.value} value={price.value}>
+                {price.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <FormControl sx={{ m: 1, minWidth: 110 }}>
+          <InputLabel>MinBeds</InputLabel>
+          <Select
+            value={filterInputs.minBeds}
+            onChange={(e) => handleInputChange(e, "minBeds")}
+            autoWidth
+            label="MinBeds"
+          >
+            {content.search.filters.bedrooms.map((beds) => (
+              <MenuItem key={"minBeds_" + beds.value} value={beds.value}>
+                {beds.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, minWidth: 110 }}>
+          <InputLabel>MaxBeds</InputLabel>
+          <Select
+            value={filterInputs.maxBeds}
+            onChange={(e) => handleInputChange(e, "maxBeds")}
+            autoWidth
+            label="MaxBeds"
+          >
+            {content.search.filters.bedrooms.map((beds) => (
+              <MenuItem key={"maxBeds_" + beds.value} value={beds.value}>
+                {beds.text}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Stack>
+      <FormControl sx={{ m: 1, minWidth: 170 }}>
+        <InputLabel>Property Type</InputLabel>
         <Select
-          value={filterInputs.maxPrice}
-          onChange={(e) => handleInputChange(e, "maxPrice")}
+          value={filterInputs.propertyType}
+          onChange={(e) => handleInputChange(e, "propertyType")}
           autoWidth
-          label="Max Price"
+          label="Property Type"
         >
-          {content.search.filters.prices.map((price) => (
-            <MenuItem value={price.value}>{price.text}</MenuItem>
+          {content.search.filters.propertyType.map((type) => (
+            <MenuItem key={"propertyType_" + type.value} value={type.value}>
+              {type.text}
+            </MenuItem>
           ))}
         </Select>
-      </Stack>
-    </FormControl>
+      </FormControl>
+    </Stack>
   );
 };
 
