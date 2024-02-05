b2ff32a1dfff4412274c09b937f3808753d67b81
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 14:03:21 +0000

    Declare types; destructure props

--- src/components/layout/ImageGrid.tsx
+++ src/components/layout/ImageGrid.tsx
@@ -1,6 +1,11 @@
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
-const ImageGrid = () => {
+type ImageGrid = {
+  imageData: { src: string; alt: string };
+  link?: string;
+};
+
+const ImageGrid = ({ imageData, link }: ImageGrid) => {
   <Grid container sx={{ height: 435 }}>
     <Grid sm={12} md={8} paddingRight={"4px"}></Grid>
     <Grid container sm={0} md={4} paddingLeft={"4px"}></Grid>
