8f1f67636a13fcbeeb8c59c650ac87229bac920e
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-05 17:31:14 +0000

    Implement media query logic; add breakpoint render condition; compose prop elements; extend types

--- src/components/layout/PrimaryGrid.tsx
+++ src/components/layout/PrimaryGrid.tsx
@@ -1,13 +1,31 @@
+"use client";
+
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import { useTheme } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-import { ListingData } from "@/types";
-
-type ListingMain = {
-  listing: ListingData;
+type PrimaryGridProps = {
+  component?: keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>;
+  constantElements: React.ReactNode;
+  mediumAboveDisplayElements: React.ReactNode;
 };
 
-const ListingMain = ({ listing }: ListingMain) => {
-  return <Grid container xs={12} sm={12} md={8}></Grid>;
+const PrimaryGrid = (props: PrimaryGridProps) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <Grid container component={props.component || "section"}>
+      {matches ? (
+        props.constantElements
+      ) : (
+        <>
+          {props.constantElements}
+          {props.mediumAboveDisplayElements}
+        </>
+      )}
+    </Grid>
+  );
 };
 
-export default ListingMain;
+export default PrimaryGrid;
