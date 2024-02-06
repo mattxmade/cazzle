ab8072bc5a6f01f1bee0279e92a6dda6f904bf3c
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-06 18:55:23 +0000

    Add icon buttons; handle open/close; create inital layout

--- src/components/ui/search/SearchFiltersModal.tsx
+++ src/components/ui/search/SearchFiltersModal.tsx
@@ -1,11 +1,43 @@
+"use client";
+
 import Dialog from "@mui/material/Dialog/Dialog";
+import DialogContent from "@mui/material/DialogContent/DialogContent";
+
+import Stack from "@mui/material/Stack/Stack";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 type SearchFiltersModal = {
   open: boolean;
+  handleClose: () => void;
 };
 
-const SearchFiltersModal = ({ open }: SearchFiltersModal) => {
-  <Dialog open={open}></Dialog>;
+const SearchFiltersModal = ({ open, handleClose }: SearchFiltersModal) => {
+  return (
+    <Dialog open={open}>
+      <Stack spacing={1}>
+        <Button onClick={handleClose} aria-label="close search form modal">
+          <CloseIcon />
+        </Button>
+
+        <DialogContent>Content</DialogContent>
+
+        <Stack direction="row" justifyContent="space-between">
+          <Button variant="outlined" aria-label="clear search filters ">
+            Clear
+          </Button>
+          <Button
+            onClick={handleClose}
+            variant="outlined"
+            aria-label="apply search filters and close search form modal"
+          >
+            Done
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
 };
 
 export default SearchFiltersModal;
