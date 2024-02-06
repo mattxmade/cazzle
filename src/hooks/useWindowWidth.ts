d87d2b302957e8daec7b63a4ff178e853f6946ad
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-06 18:24:16 +0000

    Provision window resize event listeners; handle state change; update func names

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -1,16 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+"use client";
 
-type useMediaQs = {
-  mediaQueries: number[];
+import { useEffect, useState } from "react";
+
+const useWindowWidth = () => {
+  const [currWidth, setCurrWidth] = useState<number>(0);
+
+  const handleResizeChange = () => {
+    window && setCurrWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    handleResizeChange();
+    window && window.addEventListener("resize", handleResizeChange);
+
+    // clean up function | remove EventLis when unmounted
+    return () => {
+      window && window.removeEventListener("resize", handleResizeChange);
+    };
+  }, []);
+
+  return {
+    currWidth,
+  };
 };
 
-const useMediaQs = (params: useMediaQs) => {
-  const mediaQs = useRef(
-    [...new Array(params.mediaQueries.length)].map((mq, i) => {
-      const modMq = `${params.mediaQueries[i]}px`;
-      return modMq;
-    })
-  );
-};
-
-export default useMediaQs;
+export default useWindowWidth;
