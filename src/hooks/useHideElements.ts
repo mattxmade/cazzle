fd056c41b3ae4e722cd08b65d56b8865f3f5407f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-09 23:10:30 +0000

    Complete useHideElements hook: adds type safety, settings restore + multi element handling

--- src/hooks/useHideElements.ts
+++ src/hooks/useHideElements.ts
@@ -1,7 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-const useHideElements = (elements: HTMLElement[]) => {
-  useEffect(() => {}, []);
+type ElementDisplay = React.CSSProperties["display"];
+
+const useHideElements = (elements: Array<keyof JSX.IntrinsicElements>) => {
+  const displaySettingsRef = useRef<ElementDisplay[] | []>([]);
+  const elementsRef = useRef<Array<HTMLElement | undefined> | null>(null);
+
+  useEffect(() => {
+    if (!document) return;
+    [...new Array(elements.length)].map((_, i) => {
+      const elNode = document.querySelector(elements[i]) as HTMLElement;
+
+      if (!elNode) return;
+
+      !displaySettingsRef.current.length
+        ? (displaySettingsRef.current = [elNode.style.display])
+        : (displaySettingsRef.current = [
+            ...displaySettingsRef.current,
+            elNode.style.display,
+          ]);
+
+      elNode.style.display = "none";
+      return elNode;
+    });
+
+    return () => {
+      if (!document || !elementsRef.current) return;
+
+      if (!elementsRef.current.length || !displaySettingsRef.current.length)
+        return;
+
+      elementsRef.current.forEach((el, i) => {
+        const displaySetting = displaySettingsRef.current[i];
+        if (!el || !displaySetting) return;
+
+        el.style.display = displaySetting;
+      });
+    };
+  }, []);
 };
 
 export default useHideElements;
