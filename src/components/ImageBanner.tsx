1d5528acc9a729f7191001a44f87928b33b01521
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-10 16:12:33 +0000

    Implement padding prop

--- src/components/ImageBanner.tsx
+++ src/components/ImageBanner.tsx
@@ -7,15 +7,16 @@ type ImageBannerProps = {
   alt: string;
   slug?: string;
   height?: number;
+  padding?: React.CSSProperties["padding"];
 };
 
-const ImageBanner = ({ src, alt, slug, height }: ImageBannerProps) => {
+const ImageBanner = ({ src, alt, slug, height, padding }: ImageBannerProps) => {
   return (
     <Button
       fullWidth
       href={slug}
       component={NextLink}
-      sx={{ height: height ?? 120 }}
+      sx={{ height: height ?? 120, padding: padding ?? 0 }}
     >
       <Image fill src={src} alt={alt} style={{ objectFit: "cover" }} />
     </Button>
