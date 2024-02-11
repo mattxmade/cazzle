c71d5b8288dace8232521d2de5e7de9717b12b72
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-11 16:57:03 +0000

    Add render condition logic; add NoItemCard fallback; update type props

--- src/components/layout/ImageGrid.tsx
+++ src/components/layout/ImageGrid.tsx
@@ -8,47 +8,51 @@ import Button from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-type ImageGrid = {
-  imageData: { src: string | null; alt?: string }[];
+import NoItemCard from "../NoItemCard";
+
+type ImageGridProps = {
+  imageData: { src: string | null; alt?: string }[] | null;
   link: string;
 };
 
-const ImageGrid = ({ imageData, link }: ImageGrid) => {
+const ImageGrid = ({ imageData, link }: ImageGridProps) => {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
 
-  const additionalImages =
-    imageData.length === 1
-      ? null // NoItemCard x2
-      : imageData.length === 2
-      ? [imageData[1]] // NoItemCard x1
-      : imageData.filter((data, i) => i > 0 && i < 3 && data);
+  const additionalImages = !imageData
+    ? [{ image: null }, { image: null }]
+    : imageData.length === 1
+    ? [{ image: null }]
+    : imageData.length === 2
+    ? [{ image: imageData[1] }, { image: null }]
+    : [{ image: imageData[1] }, { image: imageData[2] }];
 
   return (
     <Grid container sx={{ height: 435 }}>
       <Grid xs={12} sm={12} md={8} height="inherit" paddingRight={"4px"}>
-        {
-          imageData.length && imageData[0].src ? (
-            <Button
-              fullWidth
-              component={NextLink}
-              href={`${link + 0}`}
-              aria-label="listing image 1"
-              sx={{
-                height: "inherit",
-                overflow: "hidden",
-              }}
-            >
-              <Image
-                fill
-                priority
-                src={imageData[0].src}
-                alt={"lsting image " + 1}
-                style={{ objectFit: "cover" }}
-              />
-            </Button>
-          ) : null // TODO: <NoImageCard/>
-        }
+        <Button
+          fullWidth
+          component={NextLink}
+          href={`${link + 0}`}
+          aria-disabled={!imageData}
+          aria-label={!imageData ? "listing image 1" : "no image"}
+          sx={{
+            height: "inherit",
+            overflow: "hidden",
+          }}
+        >
+          {imageData && imageData[0]?.src ? (
+            <Image
+              fill
+              priority
+              src={imageData[0].src}
+              alt={"lsting image " + 1}
+              style={{ objectFit: "cover" }}
+            />
+          ) : (
+            <NoItemCard variant="image" />
+          )}
+        </Button>
       </Grid>
 
       {matches ? null : (
@@ -61,29 +65,33 @@ const ImageGrid = ({ imageData, link }: ImageGrid) => {
           paddingLeft={"4px"}
         >
           {additionalImages
-            ? additionalImages.map(
-                (item, i) =>
-                  item.src ? (
-                    <Button
-                      fullWidth
-                      key={"image_" + i}
-                      aria-label={"listing image " + i + 1}
-                      sx={{
-                        height: "calc(50% - 4px)",
-                        overflow: "hidden",
-                        position: "relative",
-                        alignSelf: i === 0 ? "start" : "end",
-                      }}
-                    >
-                      <Image
-                        fill
-                        src={item.src}
-                        alt={"listing image " + i + 1}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </Button>
-                  ) : null // TODO: <NoImageCard/>
-              )
+            ? additionalImages.map((item, i) => (
+                <Button
+                  fullWidth
+                  key={"image_" + i}
+                  aria-disabled={!item.image ? true : false}
+                  aria-label={
+                    item.image ? "listing image " + i + 1 : "no image"
+                  }
+                  sx={{
+                    height: "calc(50% - 4px)",
+                    overflow: "hidden",
+                    position: "relative",
+                    alignSelf: i === 0 ? "start" : "end",
+                  }}
+                >
+                  {item.image && item.image.src ? (
+                    <Image
+                      fill
+                      src={item.image.src}
+                      alt={"listing image " + i + 1}
+                      style={{ objectFit: "cover" }}
+                    />
+                  ) : (
+                    <NoItemCard variant="image" />
+                  )}
+                </Button>
+              ))
             : null}
         </Grid>
       )}
