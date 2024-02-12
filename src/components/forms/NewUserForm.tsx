b85550796c7d7b5218c02290a6388e4d849a40e9
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-12 18:47:23 +0000

    Implement Mui Form Components; complete form section; consume heading prop

--- src/components/forms/NewUserForm.tsx
+++ src/components/forms/NewUserForm.tsx
@@ -1,5 +1,45 @@
-const NewUserForm = () => {
-  return <h1>New Account Form</h1>;
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+
+type NewUserFormProps = {
+  heading: string;
+};
+
+const NewUserForm = ({ heading }: NewUserFormProps) => {
+  return (
+    <Container component="form" maxWidth="xl">
+      <Typography variant="h1" sx={{ fontSize: 48 }}>
+        {heading}
+      </Typography>
+
+      <FormControl>
+        <FormLabel id="new-user-signup">Account type</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="new-user-signup"
+          name="row-radio-buttons-group"
+        >
+          <FormControlLabel
+            value="standard"
+            control={<Radio />}
+            label="Standard"
+          />
+          <FormControlLabel
+            value="estate-agent"
+            control={<Radio />}
+            label="Estate-agent"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Container>
+  );
 };
 
 export default NewUserForm;
