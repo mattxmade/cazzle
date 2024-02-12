6c059870e546adb17efb3b81a2d2901a0114d0a3
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-12 21:57:55 +0000

    Implement Mui Stepper Component stack; setup step state

--- src/components/forms/NewUserForm.tsx
+++ src/components/forms/NewUserForm.tsx
@@ -1,44 +1,73 @@
-import Container from "@mui/material/Container";
+"use client";
+
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Stepper from "@mui/material/Stepper";
 
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
+import Input from "@mui/material/Input";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
+import { useState } from "react";
 
 type NewUserFormProps = {
   heading: string;
+  userName: string | null;
 };
 
-const NewUserForm = ({ heading }: NewUserFormProps) => {
+const NewUserForm = ({ heading, userName }: NewUserFormProps) => {
+  const [formStep, setFormStep] = useState(0);
+
   return (
-    <Container component="form" maxWidth="xl">
-      <Typography variant="h1" sx={{ fontSize: 48 }}>
+    <Stack component="section" spacing={2}>
+      <Typography variant="h1" sx={{ fontSize: 36 }}>
         {heading}
       </Typography>
 
-      <FormControl>
-        <FormLabel id="new-user-signup">Account type</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="new-user-signup"
-          name="row-radio-buttons-group"
-        >
-          <FormControlLabel
-            value="standard"
-            control={<Radio />}
-            label="Standard"
-          />
-          <FormControlLabel
-            value="estate-agent"
-            control={<Radio />}
-            label="Estate-agent"
-          />
-        </RadioGroup>
-      </FormControl>
-    </Container>
+      <Stepper activeStep={0}>
+        <Step completed={false}>
+          <StepLabel>Account Setup</StepLabel>
+        </Step>
+
+        <Step completed={false}>
+          <StepLabel>Account Options</StepLabel>
+        </Step>
+      </Stepper>
+
+      {formStep === 0 ? (
+        <Stack component="form" spacing={2}>
+          <FormControl sx={{ maxWidth: "min(100%, 20rem)" }}>
+            <FormLabel id="account-name">Name</FormLabel>
+            <Input readOnly value={userName ?? ""} />
+          </FormControl>
+          <FormControl>
+            <FormLabel id="account-type">Account type</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="account-type"
+              name="account-type-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="standard"
+                control={<Radio />}
+                label="Standard"
+              />
+              <FormControlLabel
+                value="estate-agent"
+                control={<Radio />}
+                label="Estate-agent"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Stack>
+      ) : null}
+    </Stack>
   );
 };
 
