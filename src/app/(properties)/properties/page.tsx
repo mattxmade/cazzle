dd947f1accde9e5dcbc0218a23b17ba23c649b3e
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 16:10:05 +0000

    Add hasFilters conditional; update layout

--- src/app/(properties)/properties/page.tsx
+++ src/app/(properties)/properties/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 
+import Card from "@mui/material/Card";
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import Container from "@mui/material/Container";
 
@@ -7,6 +8,7 @@ import NotFound from "@/app/not-found";
 import PropertyCards from "@/components/layout/cards/PropertyCards";
 import SearchFiltersBar from "@/components/ui/search/SearchFiltersBar";
 import { validateSearchQuery } from "@/server/validation/validateSearchQuery";
+import { customTheme } from "@/styles/custom";
 
 export type ValidParam =
   | "location"
@@ -41,22 +43,38 @@ export default async function PropertiesPage({ searchParams }: SearchParams) {
     filters &&
     filters.every((query) => validateSearchQuery(query[0], query[1]));
 
+  const hasFilters = filters.length && isValid ? true : false;
+
   return filters && !isValid ? (
     <NotFound />
   ) : (
     <>
-      <Container disableGutters maxWidth={false}>
-        <SearchFiltersBar queryParams={isValid ? searchParams : null} />
+      <Container
+        disableGutters
+        maxWidth={false}
+        sx={{
+          padding: 2,
+          backgroundColor: customTheme.backgroundColor.search.filtersBar,
+        }}
+      >
+        <Card elevation={4}>
+          <SearchFiltersBar queryParams={isValid ? searchParams : null} />
+        </Card>
       </Container>
 
       <Container
         component="main"
         maxWidth="xl"
-        sx={{ justifyContent: "center", backgroundColor: "#e9e9eb" }}
+        sx={{
+          flex: "auto",
+          justifyContent: "center",
+          padding: "32px 16px 96px",
+          backgroundColor: customTheme.backgroundColor.search.main,
+        }}
       >
         <Grid container component="section" spacing={2} padding={0} margin={0}>
           <Suspense fallback={<p>Loading...</p>}>
-            <PropertyCards search={isValid ? { searchParams } : null} />
+            <PropertyCards search={hasFilters ? { searchParams } : null} />
           </Suspense>
         </Grid>
       </Container>
