fff7c26070393c29b093f7d799bd6b8452562ce6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 15:52:55 +0000

    Complete MediaPage: handle db query; add MediaGallery Cmpnt + assing props

--- src/app/(properties)/properties/(propertyId)/[propertyId]/media/page.tsx
+++ src/app/(properties)/properties/(propertyId)/[propertyId]/media/page.tsx
@@ -1,5 +1,37 @@
-export default function MediaPage() {
-  // TODO: Get media from db
+import { api } from "@/../convex/_generated/api";
+import { Id } from "@/../convex/_generated/dataModel";
+import { fetchQuery } from "convex/nextjs";
 
-  return <MediaGallery />;
+import NotFound from "@/app/not-found";
+import type { PropertyListing_ } from "@/types";
+
+import MediaGallery from "@/components/layout/MediaGallery";
+
+type MediaPageParams = {
+  params: { propertyId: string };
+  searchParams: { media: string };
+};
+
+export default async function MediaPage(props: MediaPageParams) {
+  const { params, searchParams } = props;
+
+  // Db query
+  const query = api.properties.queries.getPropertyBySlug;
+
+  const property = (await fetchQuery(query, {
+    name: params.propertyId,
+  })) as PropertyListing_;
+
+  if (!property) return <NotFound />;
+
+  const imageData = await fetchQuery(api.properties.queries.getPropertyImages, {
+    ImageIdList: property.galleryImages as { storageId: Id<"_storage"> }[],
+  });
+
+  if (!imageData) return <NotFound />;
+
+  // initial media item to display ( origin from listing page thumbnails )
+  const item = searchParams.media ? Number(searchParams.media) : 0;
+
+  return <MediaGallery mediaData={imageData} initItem={item} />;
 }
