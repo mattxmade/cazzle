6b03f0aea321e4a520411eaad69e675a4e1c60c6
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 15:02:03 +0000

    Add Providers: Clerk Convex Mui; Create global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,49 @@
-import { ReactNode } from "react";
+import ConvexClerkProvider from "../components/providers/ConvexClerkProvider";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import theme from "@/theme";
+
+import NavBar from "@/components/core/NavBar";
+import Header from "@/components/core/Header";
+import Footer from "@/components/core/Footer";
 import "./globals.css";
 
 type RootLayoutProps = Readonly<{
-  children: ReactNode;
+  children: React.ReactNode;
 }>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ConvexClerkProvider>
+      <html lang="en">
+        <body>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Container
+                disableGutters
+                maxWidth={false}
+                sx={{
+                  minHeight: "100vh",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Header>
+                  <NavBar />
+                </Header>
+                {children}
+                <Footer />
+              </Container>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </ConvexClerkProvider>
   );
 }
