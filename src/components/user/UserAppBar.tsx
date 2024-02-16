99bd32344bde67a3d6b8f73f8c9e90b8c9a41c38
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 21:42:54 +0000

    Add Mui AppBar; set initial styling

--- src/components/user/UserAppBar.tsx
+++ src/components/user/UserAppBar.tsx
@@ -1,5 +1,22 @@
+import AppBar from "@mui/material/AppBar";
+
+import { customTheme } from "@/styles/custom";
+
 const UserAppBar = () => {
-  <h2>User App Bar</h2>;
+  return (
+    <AppBar
+      elevation={6}
+      position="fixed"
+      sx={{
+        top: "auto",
+        bottom: 0,
+        padding: 1,
+        backgroundColor: customTheme.backgroundColor.search.filtersBar,
+      }}
+    >
+      <h2>User App Bar</h2>
+    </AppBar>
+  );
 };
 
 export default UserAppBar;
