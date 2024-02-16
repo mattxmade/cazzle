00ddc8e9911d6d448a23dd300b18731695f2c467
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 00:18:19 +0000

    Complete UserFavourites RSC: construct query; map items; add Suspense

--- src/components/layout/cards/UserFavourites.tsx
+++ src/components/layout/cards/UserFavourites.tsx
@@ -1,5 +1,45 @@
-const UserFavourites = async () => {
-  return <h1>User Favourites</h1>;
+import "server-only";
+
+import { api } from "@/../convex/_generated/api";
+import { fetchQuery } from "convex/nextjs";
+import type { Id } from "@/../convex/_generated/dataModel";
+
+import { Suspense } from "react";
+import Typography from "@mui/material/Typography";
+
+import PropertyCard from "./PropertyCard";
+import type { PropertyListing_ } from "@/types";
+
+type UserFavouritesProps = {
+  id: Id<"users">;
+  token: string;
+  userFavourites: Id<"properties">[];
+};
+
+const UserFavourites = async (props: UserFavouritesProps) => {
+  const { getUserFavourites } = api.users.queries;
+
+  const args = { user_id: props.id, favourites: props.userFavourites };
+  const options = { token: props.token };
+
+  const listingItems = (await fetchQuery(
+    getUserFavourites,
+    args,
+    options
+  )) as PropertyListing_[];
+
+  return listingItems && listingItems.length ? (
+    listingItems.map((listing) => (
+      <Suspense key={listing._id}>
+        <PropertyCard
+          propertyData={listing}
+          isUserFavourite={props.userFavourites.includes(listing._id)}
+        />
+      </Suspense>
+    ))
+  ) : (
+    <Typography>No listings found</Typography>
+  );
 };
 
 export default UserFavourites;
