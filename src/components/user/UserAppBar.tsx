a53e217320831732a32f0e09b37416b4b4ea1d01
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-16 23:06:24 +0000

    Add select input + FavouritesModal Cmpnt; update layout

--- src/components/user/UserAppBar.tsx
+++ src/components/user/UserAppBar.tsx
@@ -7,9 +7,16 @@ import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
-import { customTheme } from "@/styles/custom";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
+import FavouritesModal from "./FavouritesModal";
 
 const UserAppBar = () => {
+  const selectOptions = ["default"];
+
   return (
     <AppBar
       elevation={6}
@@ -17,39 +24,68 @@ const UserAppBar = () => {
       sx={{
         top: "auto",
         bottom: 0,
-        padding: 1,
-        backgroundColor: customTheme.backgroundColor.search.filtersBar,
+        color: "#240046",
+        backgroundColor: "inherit",
       }}
     >
       <Container
         maxWidth="xl"
         sx={{
-          gap: 24,
+          gap: 16,
           display: "flex",
           flexDirection: "row",
-          position: "relative",
           justifyContent: "space-between",
+
+          padding: 1,
+          backdropFilter: "blur(2px)",
+          backgroundColor: "hsl(0 0% 100%/.2)",
         }}
       >
-        <h2>Your favourites</h2>
+        <Typography variant="h2" fontSize={28}>
+          Favourites
+        </Typography>
         <Stack
+          gap={4}
           flex="auto"
           direction="row"
           alignItems="center"
           justifyContent="space-between"
         >
           <Stack justifySelf="flex-start">
-            <Typography>List</Typography>
+            <FormControl sx={{ m: 1, minWidth: 150 }}>
+              <InputLabel>Favourite List</InputLabel>
+
+              <Select value={"default"} autoWidth label="Favourite List">
+                {selectOptions.map((listOption) => (
+                  <MenuItem key={"location_" + listOption} value={listOption}>
+                    {listOption}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Stack>
 
-          <Stack gap={1} alignItems="center" justifyContent="center">
+          <Stack
+            gap={1}
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              minWidth: "72.5px",
+            }}
+          >
             <Card
               elevation={4}
-              sx={{ width: "fit-content", borderRadius: "100%" }}
+              sx={{
+                width: "fit-content",
+
+                borderRadius: "100%",
+              }}
             >
-              <IconButton>
-                <AddIcon />
-              </IconButton>
+              <FavouritesModal>
+                <IconButton>
+                  <AddIcon />
+                </IconButton>
+              </FavouritesModal>
             </Card>
             <Typography variant="button">New List</Typography>
           </Stack>
