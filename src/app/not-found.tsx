bc4108fcb728507ca004e5c3171844bc380763ad
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-17 19:05:38 +0000

    Update NotFound: Add Mui Element Components + update content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,36 @@
-import Link from "next/link";
+import Image from "next/image";
+import NextLink from "next/link";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Stack alignItems="center" gap={4}>
+      <Box width={"100%"} height={420} sx={{ position: "relative" }}>
+        <Image
+          fill
+          priority
+          alt="404 - page not found"
+          src="/not-found.svg"
+          style={{ objectFit: "contain" }}
+        />
+      </Box>
+
+      <Stack alignItems="center" gap={2}>
+        <Stack alignItems="center">
+          <Typography variant="h1" fontSize={36}>
+            Page Not Found
+          </Typography>
+          <Typography>Could not find requested resource</Typography>
+        </Stack>
+
+        <Button variant="outlined" href="/" LinkComponent={NextLink}>
+          Return Home
+        </Button>
+      </Stack>
+    </Stack>
   );
 }
