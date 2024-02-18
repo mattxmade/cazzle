0f27690fee2a9431b0d00255ef88feffc7c25e8f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-18 22:46:16 +0000

    Progress layout; add Mui AppBar + content

--- src/app/(dashboard)/dashboard/_components/DashboardModal.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardModal.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent/DialogContent";
 import SlideTransition from "@/components/mui/transitions/SlideTransition";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+
+import Stack from "@mui/material/Stack/Stack";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
 
 type DashboardModalProps = {
   open: boolean;
@@ -19,7 +29,26 @@ const DashboardModal = (props: DashboardModalProps) => {
       onClose={handleCloseModal}
       TransitionComponent={SlideTransition}
     >
-      {children}
+      <AppBar sx={{ position: "relative" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleCloseModal}
+            aria-label="close property editor modal"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            Property Edit
+          </Typography>
+          <Button autoFocus color="inherit" onClick={handleCloseModal}>
+            save
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <DialogContent>{children}</DialogContent>
     </Dialog>
   );
 };
