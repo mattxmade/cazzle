c8c12ad6cee6eb44b7d492b0ed4f5fdd1e62e2dd
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-20 23:24:27 +0000

    Delcare Component types; provision state; create handler func

--- src/components/ui/Concertina.tsx
+++ src/components/ui/Concertina.tsx
@@ -1,6 +1,40 @@
 "use client";
 
-const Concertina = () => {
+import React, { useState } from "react";
+
+import Accordion, {
+  AccordionProps,
+  AccordionSlots,
+} from "@mui/material/Accordion";
+
+import AccordionSummary, {
+  type AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+
+import AccordionDetails, {
+  type AccordionDetailsProps,
+} from "@mui/material/AccordionDetails";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+type MuiProps = {
+  accordionProps?: AccordionProps;
+  accordionSummaryProps?: AccordionSummaryProps;
+  accordionDetailsProps?: AccordionDetailsProps;
+};
+
+type ConcertinaProps = {
+  muiProps: MuiProps;
+  children?: React.ReactNode;
+};
+
+const Concertina = (props: ConcertinaProps) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpansion = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
+
   return <h2>Concertina</h2>;
 };
 
