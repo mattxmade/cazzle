ca8801e6f1df77d29bbd2d00ac467dde55dda06e
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-20 15:03:02 +0000

    Configure props; add Mui Select family Component elements; consume props

--- src/components/forms/inputs/SelectDropdown.tsx
+++ src/components/forms/inputs/SelectDropdown.tsx
@@ -1,7 +1,47 @@
 "use client";
 
-const SelectDropdown = () => {
-  return <h2>Select Dropdown</h2>;
+import Select, { type SelectProps } from "@mui/material/Select";
+import InputLabel, { type InputLabelProps } from "@mui/material/InputLabel";
+import FormControl, { type FormControlProps } from "@mui/material/FormControl";
+
+type Props = {
+  id: string;
+  label: string;
+  defaultValue?: string;
+  formControlProps?: FormControlProps;
+  inputLabelProps?: InputLabelProps;
+  selectProps?: SelectProps;
+  children?: React.ReactNode;
+  handleUpdateFormRef: () => void;
+};
+
+const SelectDropdown = (props: Props) => {
+  return (
+    <FormControl
+      required
+      variant="outlined"
+      sx={{ width: 210 }}
+      {...props.formControlProps}
+    >
+      <InputLabel htmlFor={props.id} {...props.inputLabelProps}>
+        {props.label}
+      </InputLabel>
+      <Select
+        autoWidth
+        id={props.id}
+        label={props.label}
+        value={props.defaultValue ?? ""}
+        SelectDisplayProps={{
+          style: {
+            backgroundColor: "inherit",
+          },
+        }}
+        {...props.selectProps}
+      >
+        {props.children}
+      </Select>
+    </FormControl>
+  );
 };
 
 export default SelectDropdown;
