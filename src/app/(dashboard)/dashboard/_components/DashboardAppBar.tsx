fddbb1b20c11a9486393f5b308f9bf0b24edc9ff
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-21 01:26:11 +0000

    Update props; cleanup Component

--- src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardAppBar.tsx
@@ -2,39 +2,37 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-import MuiAppBar, {
-  type AppBarProps as MuiAppBarProps,
-} from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-
-import { styled } from "@mui/material/styles";
-import { useState } from "react";
+import AppBar from "@/components/mui/app-bar/AppBar";
 
 type DashboardAppBarProps = {
   open?: boolean;
   title: string;
+  drawerWidth: 240;
   handleDrawerOpen: () => void;
 };
 
 const DashboardAppBar = (props: DashboardAppBarProps) => {
-  const { open, title, handleDrawerOpen } = props;
+  const { open, title, drawerWidth, handleDrawerOpen } = props;
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" open={open} drawerWidth={240}>
+      <AppBar open={open} width={drawerWidth}>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="dashboard menu"
-            sx={{ marginRight: 2, ...(open && { display: "none" }) }}
+            aria-label="open drawer"
             onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
           >
             <MenuIcon />
           </IconButton>
@@ -58,27 +56,3 @@ const DashboardAppBar = (props: DashboardAppBarProps) => {
 };
 
 export default DashboardAppBar;
-
-type AppBarProps = {
-  drawerWidth: number;
-  open?: boolean;
-} & MuiAppBarProps;
-
-// https://mui.com/material-ui/react-drawer/#mini-variant-drawer
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open, drawerWidth }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
