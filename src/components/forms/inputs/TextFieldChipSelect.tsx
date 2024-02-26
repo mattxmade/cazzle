22d47d8fe7cdd05b75735349bc010e785c4071a7
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-26 19:16:43 +0000

    Implement style conditions; Create handler funcs; Add name prop

--- src/components/forms/inputs/TextFieldChipSelect.tsx
+++ src/components/forms/inputs/TextFieldChipSelect.tsx
@@ -1,29 +1,47 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 
 type ComponentProps = {
+  name: string;
   chipValues: string[];
 };
 
-const TextFieldChipSelect = ({ chipValues }: ComponentProps) => {
-  const [currentValue, setCurrentValue] = useState<string | null>(null);
+const TextFieldChipSelect = ({ name, chipValues }: ComponentProps) => {
+  const mode = useRef<"edit" | "add" | "">("");
+  const [currentValue, setCurrentValue] = useState<string>("");
+
+  const handleChipValueSelection = (e: React.MouseEvent<HTMLDivElement>) => {
+    const chip = e.currentTarget;
+
+    if (!chip.textContent) return;
+    if (!chipValues.includes(chip.textContent)) return;
+
+    mode.current = "edit";
+    chip.textContent && setCurrentValue(chip.textContent);
+  };
+
+  const handleUpdateTextField = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const textFieldInput = e.currentTarget;
+    setCurrentValue(textFieldInput.value);
+  };
 
   return (
     <>
       <Stack gap={2} direction="row" alignItems="flex-end">
         <TextField
           required
-          label={currentValue ? "Edit feature" : "New feature"}
-          id="feature"
+          focused={currentValue ? true : false}
+          label={currentValue ? `Edit ${name}` : `New ${name}`}
+          id={name}
           size="small"
           value={currentValue}
-          onChange={() => {}}
-          placeholder={!currentValue ? "Enter feature" : ""}
+          onChange={handleUpdateTextField}
+          placeholder={!currentValue ? `Edit ${name}` : ""}
         />
       </Stack>
 
@@ -32,8 +50,16 @@ const TextFieldChipSelect = ({ chipValues }: ComponentProps) => {
           <Chip
             key={`${value}-${i}`}
             label={value}
-            onClick={() => {}}
+            onClick={handleChipValueSelection}
             onDelete={() => {}}
+            sx={{
+              color: currentValue === value ? "HighlightText" : "ButtonText",
+              backgroundColor: currentValue === value ? "Highlight" : "Button",
+              ":hover": {
+                backgroundColor:
+                  currentValue === value ? "Highlight" : "Button",
+              },
+            }}
           />
         ))}
       </Stack>
