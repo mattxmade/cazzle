072ad6392adea099c596a26e2fac44582d03c370
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-26 18:31:50 +0000

    Implement Mui Chip TextField Stack; add state + consume props

--- src/components/forms/inputs/TextFieldChipSelect.tsx
+++ src/components/forms/inputs/TextFieldChipSelect.tsx
@@ -1,5 +1,44 @@
-const TextFieldChipSelect = () => {
-  return <h2>Text Field Chip Select</h2>;
+"use client";
+
+import React, { useState } from "react";
+
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+
+type ComponentProps = {
+  chipValues: string[];
+};
+
+const TextFieldChipSelect = ({ chipValues }: ComponentProps) => {
+  const [currentValue, setCurrentValue] = useState<string | null>(null);
+
+  return (
+    <>
+      <Stack gap={2} direction="row" alignItems="flex-end">
+        <TextField
+          required
+          label={currentValue ? "Edit feature" : "New feature"}
+          id="feature"
+          size="small"
+          value={currentValue}
+          onChange={() => {}}
+          placeholder={!currentValue ? "Enter feature" : ""}
+        />
+      </Stack>
+
+      <Stack gap={1} direction="row" flexWrap="wrap">
+        {chipValues.map((value, i) => (
+          <Chip
+            key={`${value}-${i}`}
+            label={value}
+            onClick={() => {}}
+            onDelete={() => {}}
+          />
+        ))}
+      </Stack>
+    </>
+  );
 };
 
 export default TextFieldChipSelect;
