b327c7047408d589bef02b31e076a90d34b68b7a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-29 16:30:45 +0000

    Complete AlertDialog; implement Mui Dialog family; declare types + consume props

--- src/components/ui/feedback/AlertDialog.tsx
+++ src/components/ui/feedback/AlertDialog.tsx
@@ -1,5 +1,38 @@
-const AlertDialog = () => {
-  return <h2>AlertDialog</h2>;
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+type AlertProps = {
+  open: boolean;
+  title: string;
+  desc?: string;
+  handleClose: () => void;
+  children: React.ReactNode;
+};
+
+const AlertDialog = (props: AlertProps) => {
+  return (
+    <>
+      <Dialog
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
+        <DialogContent>
+          {props.desc ? (
+            <DialogContentText id="alert-dialog-description">
+              {props.desc}
+            </DialogContentText>
+          ) : null}
+        </DialogContent>
+        <DialogActions>{props.children}</DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export default AlertDialog;
