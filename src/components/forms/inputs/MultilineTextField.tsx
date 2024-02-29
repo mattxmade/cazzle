64721320e1569486d20ebc95a91d8bd2a4b95d05
Matt <97047080+mattxmade@users.noreply.github.com>
2024-02-29 18:56:35 +0000

    Implement basic text input validation; update props

--- src/components/forms/inputs/MultilineTextField.tsx
+++ src/components/forms/inputs/MultilineTextField.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
+
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
 import InputLabel, { type InputLabelProps } from "@mui/material/InputLabel";
 import FormControl, { type FormControlProps } from "@mui/material/FormControl";
 
-import { useState } from "react";
 import generateLabelName from "@/utils/generateLabelName";
+import { type Pattern, isValidTextInput } from "@/utils/validateInputs";
 
 type MultilineTextFieldProps = {
   id: string;
@@ -25,6 +27,11 @@ const MultilineTextField = (props: MultilineTextFieldProps) => {
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.currentTarget;
 
+    const paragraphs = input.value.split("\n\n\n");
+    if (paragraphs.some((p) => !isValidTextInput(p, props.id as Pattern))) {
+      return;
+    }
+
     props.handleUpdateFormRef(props.id, input.value);
     setText(input.value);
   };
@@ -44,12 +51,12 @@ const MultilineTextField = (props: MultilineTextFieldProps) => {
       {...props.formControlProps}
     >
       <TextField
-        {...layout}
         id={labelName}
-        variant="outlined"
-        defaultValue={text}
+        value={text}
         placeholder={props.label}
         onChange={handleTextChange}
+        variant="outlined"
+        {...layout}
         {...props.textFieldProps}
       />
     </FormControl>
