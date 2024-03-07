310f1c2b2a5d8993c72f0fb65ffab1aa45fd6f59
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-07 01:13:05 +0000

    Complete Backdrop Component

--- src/components/ui/feedback/Backdrop.tsx
+++ src/components/ui/feedback/Backdrop.tsx
@@ -1,5 +1,19 @@
-const Backdrop = () => {
-  return <h2>Backdrop</h2>;
+import MuiBackdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+type Props = {
+  open: boolean;
+};
+
+const Backdrop = ({ open }: Props) => {
+  return (
+    <MuiBackdrop
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+    >
+      <CircularProgress color="inherit" />
+    </MuiBackdrop>
+  );
 };
 
 export default Backdrop;
