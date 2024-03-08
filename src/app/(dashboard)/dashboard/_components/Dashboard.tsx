3cf9abdfadf4fabf36c00ea2ddc0ce4d398e4722
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-08 01:41:11 +0000

    Handle formData via useTransition; add pending render conditions

--- src/app/(dashboard)/dashboard/_components/Dashboard.tsx
+++ src/app/(dashboard)/dashboard/_components/Dashboard.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState, useTransition } from "react";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 import Box from "@mui/material/Box";
 import AdaptiveBox from "@/components/mui/box/AdaptiveBox";
 import ElevationScroll from "@/components/mui/ElevationOnScroll";
 
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 
 import MenuItems from "./MenuItems";
@@ -24,11 +24,15 @@ import DashboardItem from "./DashboardItem";
 
 import PropertyEditor from "./editor/PropertyEditor";
 import AlertDialog from "@/components/ui/feedback/AlertDialog";
+import Backdrop from "@/components/ui/feedback/Backdrop";
 
 import theme from "@/theme";
 import { customTheme } from "@/styles/custom";
-import type { PropertyListing_ } from "@/types";
+
 import { validate } from "@/data/validate";
+import type { PropertyListing_ } from "@/types";
+import { type DbResponse } from "@/data/dbStatus";
+import { type AgentModel, updateDocument } from "@/server/actions/agent/agent";
 
 export type FormDataRef = {
   type: string;
@@ -57,20 +61,23 @@ type DashboardProps = {
 const menuDrawerWidth = 240;
 
 const Dashboard = (props: DashboardProps) => {
+  const [pending, startTransition] = useTransition();
+  const [serverResponse, setServerResponse] = useState<DbResponse | null>(null);
+
   const [open, setOpen] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
   const [view, setView] = useState<string>("properties-view");
 
-  const [openModal, setOpenModal] = useState(false);
   const initialData = useRef<FormDataRef>(null);
   const unsavedData = useRef<Partial<PropertyListing_> | null>(null);
+
+  const alertMessage = useRef<{ title: string; desc?: string } | null>(null);
+  const [showAlert, setShowAlert] = useState(false);
 
   const [formErrors, setFormErrors] = useState<{
     for: "property" | "listing";
     errors: PropertyErrors;
   } | null>(null);
-
-  const alertMessage = useRef<{ title: string; desc?: string } | null>(null);
-  const [showAlert, setShowAlert] = useState(false);
 
   const handleUpdateLocalData: UpdateFormDataFuncion = (type, updatedData) => {
     if (!initialData.current || !updatedData) return;
@@ -93,13 +100,24 @@ const Dashboard = (props: DashboardProps) => {
     unsavedData.current = valuesToUpdate;
   };
 
-  let canUpdate = false;
+  const handleSaveChanges = async (
+    e: React.PointerEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
 
-  const handleSaveChanges = async () => {
+    if (pending) return;
     showAlert && setShowAlert(false);
 
     if (!initialData.current || !unsavedData.current) return;
-    const data = { ...initialData.current.data, ...unsavedData.current };
+
+    // TODO
+    const documentType =
+      initialData.current.type === "Property" ? "properties" : "agents";
+
+    const data = {
+      ...initialData.current.data,
+      ...unsavedData.current,
+    } as PropertyListing_;
 
     const result = validate("property", data);
 
@@ -109,11 +127,23 @@ const Dashboard = (props: DashboardProps) => {
       return;
     }
 
-    // TODO: implement db patch
-    // await update db
+    const formData = new FormData();
 
-    formErrors && setFormErrors(null);
-    canUpdate && handleClearData();
+    formData.append("type", documentType);
+    formData.append("data", JSON.stringify(data));
+
+    startTransition(async () => {
+      const response = await updateDocument(formData);
+      console.log(response);
+
+      response && setServerResponse(response);
+      response.status === "success" ? completeDashboardTask() : null;
+    });
+  };
+
+  const completeDashboardTask = () => {
+    handleClearData();
+    handleCloseAllDialogs();
   };
 
   const handleCloseWithoutSaving = () => {
@@ -214,54 +244,65 @@ const Dashboard = (props: DashboardProps) => {
           backgroundColor: customTheme.backgroundColor.dashboard.main,
         }}
       >
-        <AdaptiveBox
-          reduce={open}
-          width={menuDrawerWidth}
-          margin={65}
-          sx={{
-            gap: theme.spacing(1),
-            display: "grid",
-            flex: "auto",
-            alignContent: "flex-start",
-            marginTop: 9,
-            marginBottom: 2,
-          }}
-        >
-          <DashboardView viewName="properties-view" currentView={view}>
-            {properties?.length ? (
-              properties.map((propertyData) => (
-                <DashboardItem key={propertyData._id} item={propertyData}>
-                  <Button
-                    aria-label="edit property"
-                    onClick={() => handleOpenModal("Property", propertyData)}
-                  >
-                    <BorderColorOutlinedIcon />
-                  </Button>
-                </DashboardItem>
-              ))
-            ) : (
-              <Box
-                flex="auto"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Typography variant="h6" color="darkslategrey">
-                  Properties will be listed here - press the + icon to add your
-                  first property
-                </Typography>
-              </Box>
-            )}
-          </DashboardView>
-        </AdaptiveBox>
+        <SignedOut>
+          <Backdrop open={true} />
+        </SignedOut>
+
+        <SignedIn>
+          <AdaptiveBox
+            reduce={open}
+            width={menuDrawerWidth}
+            margin={65}
+            sx={{
+              gap: theme.spacing(1),
+              display: "grid",
+              flex: "auto",
+              alignContent: "flex-start",
+              marginTop: 9,
+              marginBottom: 2,
+            }}
+          >
+            <DashboardView viewName="properties-view" currentView={view}>
+              {properties?.length ? (
+                properties.map((propertyData) => (
+                  <DashboardItem key={propertyData._id} item={propertyData}>
+                    <Button
+                      aria-label="edit property"
+                      onClick={() => handleOpenModal("Property", propertyData)}
+                    >
+                      <BorderColorOutlinedIcon />
+                    </Button>
+                  </DashboardItem>
+                ))
+              ) : (
+                <Box
+                  flex="auto"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography variant="h6" color="darkslategrey">
+                    Properties will be listed here - press the + icon to add
+                    your first property
+                  </Typography>
+                </Box>
+              )}
+            </DashboardView>
+          </AdaptiveBox>
+        </SignedIn>
       </Box>
 
       <DashboardModal
         title={initialData.current?.type}
         handleCloseModal={handleCloseModal}
         action={
-          <Button autoFocus color="inherit" onClick={handleSaveChanges}>
-            Save
+          <Button
+            autoFocus
+            color="inherit"
+            onClick={handleSaveChanges}
+            sx={{ ":hover": { cursor: !pending ? "pointer" : "auto" } }}
+          >
+            {!pending ? "Save" : <CircularProgress color="inherit" size={25} />}
           </Button>
         }
         muiProps={{
