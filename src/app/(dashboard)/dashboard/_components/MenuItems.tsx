68eaac50fcc633fc7292c8aaf234a033b650000a
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-10 23:36:52 +0000

    Add Icon prop; implement onClick callback; add aria-label

--- src/app/(dashboard)/dashboard/_components/MenuItems.tsx
+++ src/app/(dashboard)/dashboard/_components/MenuItems.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -5,19 +7,24 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
 
 type MenuItemsProps = {
-  menuItems: { name: string; text: string; icon: string }[];
+  menuItems: { name: string; text: string; Icon: React.ReactNode }[];
   handleMenuItem: (name: string) => void;
 };
 
 const MenuItems = ({ menuItems, handleMenuItem }: MenuItemsProps) => {
   return (
     <List>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon></ListItemIcon>
-          <ListItemText primary={"Menu Item"} />
-        </ListItemButton>
-      </ListItem>
+      {menuItems.map((menuItem, i) => (
+        <ListItem key={menuItem.name} disablePadding>
+          <ListItemButton
+            aria-label={menuItem.text.toLowerCase()}
+            onClick={() => handleMenuItem(menuItem.name)}
+          >
+            <ListItemIcon>{menuItem.Icon}</ListItemIcon>
+            <ListItemText primary={menuItem.text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
     </List>
   );
 };
