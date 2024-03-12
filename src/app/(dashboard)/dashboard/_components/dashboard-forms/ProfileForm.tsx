6fa5c7620fa66c66385b181ffc10dbf72f2b75c7
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-12 22:30:30 +0000

    Create form layout; add inputs

--- src/app/(dashboard)/dashboard/_components/dashboard-forms/ProfileForm.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard-forms/ProfileForm.tsx
@@ -1,5 +1,134 @@
+import Stack from "@mui/material/Stack";
+import { type TextFieldProps } from "@mui/material";
+
+import FormCard from "@/components/forms/FormCard";
+import FormSection from "@/components/forms/FormSection";
+import MultilineTextField from "@/components/forms/inputs/MultilineTextField";
+
+const textFieldProps: TextFieldProps = {
+  required: false,
+};
+
 const ProfileForm = () => {
-  return <h2>Profile Form</h2>;
+  return (
+    <Stack gap={2}>
+      <FormCard>
+        <FormSection heading="Details">
+          <MultilineTextField
+            id="branch-name"
+            label="Branch name"
+            validation="lettersWithHyphen"
+            handleUpdateFormRef={() => {}}
+          />
+
+          <MultilineTextField
+            id="telephone"
+            label="Telephone"
+            validation="number"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+
+          <MultilineTextField
+            id="email"
+            label="Email"
+            validation="lettersOnly"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+
+          <MultilineTextField
+            id="website"
+            label="Website"
+            validation="lettersOnly"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+        </FormSection>
+      </FormCard>
+
+      <FormCard>
+        <FormSection heading="Address">
+          <MultilineTextField
+            id="street-number"
+            label="Street number"
+            validation="number"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+          <MultilineTextField
+            id="street"
+            label="Street"
+            validation="lettersWithHyphen"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+          <MultilineTextField
+            id="town"
+            label="Town/City"
+            validation="lettersWithHyphen"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+          <MultilineTextField
+            id="postcode"
+            label="Postcode"
+            validation="postcode"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+        </FormSection>
+      </FormCard>
+
+      <FormCard>
+        <FormSection heading="About" stackProps={{ direction: "column" }}>
+          <MultilineTextField
+            id="summary"
+            label="Summary"
+            validation="singleline"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps, fullWidth: true }}
+          />
+
+          <MultilineTextField
+            id="description"
+            label="Description"
+            validation="multiline"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps, fullWidth: true }}
+          />
+        </FormSection>
+      </FormCard>
+
+      <FormCard>
+        <FormSection heading="Media">
+          <MultilineTextField
+            id="banner"
+            label="Banner"
+            validation="multiline"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+
+          <MultilineTextField
+            id="logo"
+            label="Logo"
+            validation="singleline"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+
+          <MultilineTextField
+            id="banner"
+            label="Banner"
+            validation="singleline"
+            handleUpdateFormRef={() => {}}
+            textFieldProps={{ ...textFieldProps }}
+          />
+        </FormSection>
+      </FormCard>
+    </Stack>
+  );
 };
 
 export default ProfileForm;
