c712cdda809819be9e9df74391aed60ab3455f82
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-19 18:54:16 +0000

    Implement Mui Components; create initial UI layout, types + props

--- src/app/(dashboard)/dashboard/_components/dashboard-forms/MediaAsset.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard-forms/MediaAsset.tsx
@@ -1,5 +1,83 @@
-const MediaAsset = () => {
-  return <h2>Media Asset</h2>;
+import Image from "next/image";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack/Stack";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup/ButtonGroup";
+
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import DeleteIcon from "@mui/icons-material/Delete";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+
+import NoItemCard from "@/components/NoItemCard";
+
+type MediaAssetProps = {
+  title: string;
+  Icon?: React.ReactNode;
+  image?: { src: string; alt: string };
+  label?: string;
+};
+
+const MediaAsset = (props: MediaAssetProps) => {
+  return (
+    <Stack width={210} alignItems="center" spacing={1}>
+      <Typography variant="button">{props.title}</Typography>
+
+      <Box
+        width={110}
+        height={110}
+        position="relative"
+        overflow="hidden"
+        borderRadius={1}
+      >
+        {props.image ? (
+          <Image
+            fill
+            src={props.image.src}
+            alt={props.image.alt}
+            style={{ objectFit: "cover" }}
+          />
+        ) : (
+          <NoItemCard variant="image" />
+        )}
+      </Box>
+
+      <ButtonGroup>
+        <Tooltip title={`Add ${props.title}`}>
+          <Button variant="contained" aria-label="add asset">
+            <AddPhotoAlternateIcon />
+          </Button>
+        </Tooltip>
+
+        <Tooltip title={`Upload ${props.title}`}>
+          <Button variant="contained" color="info" aria-label="upload asset">
+            <Input
+              aria-hidden
+              type="file"
+              name="file"
+              inputProps={{ accept: "image/jpg" }}
+              sx={{ display: "none" }}
+            />
+            <UploadFileIcon />
+          </Button>
+        </Tooltip>
+
+        <Tooltip title={`Remove ${props.title}`}>
+          <Button
+            variant="contained"
+            color="error"
+            aria-label="delete asset"
+            sx={{ width: "fit-content" }}
+          >
+            <DeleteIcon />
+          </Button>
+        </Tooltip>
+      </ButtonGroup>
+    </Stack>
+  );
 };
 
 export default MediaAsset;
