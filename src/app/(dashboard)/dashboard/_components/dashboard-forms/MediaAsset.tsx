08c82094cbe45d6c53a6914253601b81bd430ea0
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-19 23:38:04 +0000

    Fix Tooltip issue when Button disabled; Configure file Input Button

--- src/app/(dashboard)/dashboard/_components/dashboard-forms/MediaAsset.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard-forms/MediaAsset.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRef, useState } from "react";
 
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack/Stack";
@@ -22,6 +23,9 @@ type MediaAssetProps = {
 };
 
 const MediaAsset = (props: MediaAssetProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+
   return (
     <Stack width={210} alignItems="center" spacing={1}>
       <Typography variant="button">{props.title}</Typography>
@@ -47,33 +51,56 @@ const MediaAsset = (props: MediaAssetProps) => {
 
       <ButtonGroup>
         <Tooltip title={`Add ${props.title}`}>
-          <Button variant="contained" aria-label="add asset">
+          <Button component="label" variant="contained" aria-label="add asset">
             <Input
               aria-hidden
+              ref={inputRef}
               type="file"
               name="file"
               inputProps={{ accept: "image/jpg" }}
-              sx={{ display: "none" }}
+              sx={{
+                clip: "rect(0 0 0 0)",
+                clipPath: "inset(50%)",
+                height: 1,
+                overflow: "hidden",
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                whiteSpace: "nowrap",
+                width: 1,
+              }}
             />
             <AddPhotoAlternateIcon />
           </Button>
         </Tooltip>
 
         <Tooltip title={`Upload ${props.title}`}>
-          <Button
-            type="submit"
-            color="info"
-            variant="contained"
-            aria-label="upload asset"
-          >
-            <UploadFileIcon />
-          </Button>
+          <Box>
+            <Button
+              disabled={!file}
+              type="submit"
+              color="info"
+              variant="contained"
+              aria-label="upload asset"
+              aria-disabled={!file}
+            >
+              <UploadFileIcon />
+            </Button>
+          </Box>
         </Tooltip>
 
         <Tooltip title={`Remove ${props.title}`}>
-          <Button variant="contained" color="error" aria-label="delete asset">
-            <DeleteIcon />
-          </Button>
+          <Box>
+            <Button
+              disabled={!props.image}
+              variant="contained"
+              color="error"
+              aria-label="delete asset"
+              aria-disabled={!props.image}
+            >
+              <DeleteIcon />
+            </Button>
+          </Box>
         </Tooltip>
       </ButtonGroup>
     </Stack>
