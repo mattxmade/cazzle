20253ffe55a29585bcda84e33c318db919b2e710
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-20 23:15:28 +0000

    Implement React Hooks: useState + useRef

--- src/components/forms/hooks/useMediaAsset.ts
+++ src/components/forms/hooks/useMediaAsset.ts
@@ -1,9 +1,15 @@
+import { useCallback, useState } from "react";
+
 type UseMediaAssetParams = {
   name: string;
   asset?: { src: string; alt: string };
   accepts: "image/jpg";
 };
 
-const useMediaAsset = (params: UseMediaAssetParams) => {};
+const useMediaAsset = (params: UseMediaAssetParams) => {
+  const [blob, setBlob] = useState(params.asset ?? null);
+  const [file, setFile] = useState<File | null>(null);
+  const [lastFile, setLastFile] = useState<File | null>(null);
+};
 
 export default useMediaAsset;
