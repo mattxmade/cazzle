bd29122cdf34be2a04de64b686a8cff9f9724cdc
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-20 23:06:26 +0000

    Create types

--- src/components/forms/hooks/useMediaAsset.ts
+++ src/components/forms/hooks/useMediaAsset.ts
@@ -1,3 +1,9 @@
-const useMediaAsset = () => {};
+type UseMediaAssetParams = {
+  name: string;
+  asset?: { src: string; alt: string };
+  accepts: "image/jpg";
+};
+
+const useMediaAsset = (params: UseMediaAssetParams) => {};
 
 export default useMediaAsset;
