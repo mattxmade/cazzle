5edbfddd9d56bb526270a75d45aca1d516f60ab1
Matt <97047080+mattxmade@users.noreply.github.com>
2024-03-20 23:26:07 +0000

    Create file handler functions; implement cleanup function

--- src/components/forms/hooks/useMediaAsset.ts
+++ src/components/forms/hooks/useMediaAsset.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 type UseMediaAssetParams = {
   name: string;
@@ -10,6 +10,44 @@ const useMediaAsset = (params: UseMediaAssetParams) => {
   const [blob, setBlob] = useState(params.asset ?? null);
   const [file, setFile] = useState<File | null>(null);
   const [lastFile, setLastFile] = useState<File | null>(null);
+
+  const handleChooseFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileInput = e.currentTarget as HTMLInputElement;
+    const { files } = fileInput;
+
+    if (!files || !files.length) return;
+
+    const fileData = files[0];
+    const blobUrl = URL.createObjectURL(files[0]);
+
+    fileData && setFile(fileData);
+    blobUrl && setBlob({ src: blobUrl, alt: params.name });
+
+    const input = e.currentTarget as HTMLInputElement;
+    input.value = "";
+  };
+
+  const handleUploadFile = () => {
+    if (!file) return;
+    setLastFile(file);
+  };
+
+  const handleRevokeObjectUrl = () => {
+    blob && URL.revokeObjectURL(blob.src);
+  };
+
+  const handleRemoveFile = () => {
+    handleRevokeObjectUrl();
+    setBlob(null);
+  };
+
+  useEffect(handleRevokeObjectUrl, [blob]);
+
+  return {
+    handleChooseFile,
+    handleUploadFile,
+    handleRemoveFile,
+  };
 };
 
 export default useMediaAsset;
