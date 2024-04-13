dc3d72fc657c5c987f2e4fb1b8da207a74b88bc0
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-13 16:24:07 +0100

    Create ShareButton Component: implement Mui Cmpnts; initial commit

---
+++ src/components/ui/buttons/ShareButton.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+
+const ShareButton = () => (
+  <Button>
+    <ShareOutlinedIcon />
+  </Button>
+);
+
+export default ShareButton;
