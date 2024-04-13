5d924d22bef26ef274caff534c20c12f3ee71f69
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-13 16:42:15 +0100

    Implement Mui Components; handle onChange; add content

--- src/components/forms/new-user-form-steps/BranchAccountStep.tsx
+++ src/components/forms/new-user-form-steps/BranchAccountStep.tsx
@@ -1,7 +1,60 @@
 "use client";
 
-const BranchAccountStep = () => {
-  return <h2>Branch Account Step</h2>;
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import FormControl from "@mui/material/FormControl";
+
+import { content } from "@/app/content";
+
+type BranchAccountStepProps = {
+  branchNameValue: string;
+  children?: React.ReactNode;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const BranchAccountStep = (props: BranchAccountStepProps) => {
+  return (
+    <Stack component="section" spacing={3}>
+      <Card
+        elevation={4}
+        sx={{
+          padding: 4,
+          marginLeft: 2,
+          backdropFilter: "blur(2px)",
+          backgroundColor: "hsl(0 0% 100%/.6)",
+        }}
+      >
+        <Stack component="form" spacing={2}>
+          <Typography variant="h2" fontWeight={500} fontSize={24}>
+            {content.forms.account.branch.title}
+          </Typography>
+
+          <Typography variant="body1" width={"min(100%, 60ch)"}>
+            {content.forms.account.branch.description}
+          </Typography>
+
+          <FormControl sx={{ maxWidth: "min(100%, 20rem)" }}>
+            <TextField
+              required
+              label="Branch name"
+              variant="standard"
+              value={props.branchNameValue}
+              onChange={props.handleInputChange}
+            />
+          </FormControl>
+        </Stack>
+      </Card>
+
+      <Typography variant="subtitle2" sx={{ paddingLeft: 1 }}>
+        {content.forms.account.branch.note}
+      </Typography>
+
+      <ButtonGroup>{props.children}</ButtonGroup>
+    </Stack>
+  );
 };
 
 export default BranchAccountStep;
