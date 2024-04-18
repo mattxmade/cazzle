bcfe82942b86a19d2090e1aa45f9ab0ffc3dc18f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-18 23:19:48 +0100

    Complete welcome page; provision user account; add response flow

---
+++ src/app/welcome/[[...welcome]]/page.tsx
@@ -0,0 +1,75 @@
+import NextLink from "next/link";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+import createAccount from "@/server/user/createAccount";
+import getSignedInUser from "@/server/user/getUser";
+import { checkPermission } from "@/server/permissions";
+
+const flexLayout: React.CSSProperties = {
+  flex: "auto",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+};
+
+export default async function OnboardingPage() {
+  checkPermission();
+
+  const user = await getSignedInUser();
+  if (!user) redirect("/sign-in");
+
+  if (user.current?.role === "user") redirect("/properties");
+  if (user.current?.role === "estate-agent") redirect("/dashboard");
+
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const formData = new FormData();
+  formData.append("accountName", clerkUser.firstName ?? "user");
+  formData.append("accountType", "standard");
+
+  const accountSetupResponse = await createAccount(formData);
+  if (!accountSetupResponse) redirect("/sign-in");
+
+  return (
+    <Container
+      disableGutters
+      component="main"
+      maxWidth={false}
+      sx={{
+        ...flexLayout,
+        flex: "auto",
+        backgroundColor: "#e9e9eb",
+      }}
+    >
+      <Container component="section" maxWidth="xl" sx={{ ...flexLayout }}>
+        <Stack gap={6} flex="auto" alignItems="center" justifyContent="center">
+          <Typography variant="h2">{accountSetupResponse.message}</Typography>
+
+          <Typography variant="button">
+            Look for the{" "}
+            {
+              <FavoriteBorderIcon
+                aria-label="favourite icon"
+                role="presentation"
+                sx={{ top: 5, position: "relative" }}
+              />
+            }{" "}
+            next to each listing to add them to your favourites.
+          </Typography>
+
+          <Button variant="contained" component={NextLink} href="/properties">
+            View Properites
+          </Button>
+        </Stack>
+      </Container>
+    </Container>
+  );
+}
