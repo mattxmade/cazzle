189f95fa5285b88266429f03250a7f9665fd2bd4
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-20 22:16:22 +0100

    Update next.config.ts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.CONVEX_HOSTNAME,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    imageSizes: [320],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
