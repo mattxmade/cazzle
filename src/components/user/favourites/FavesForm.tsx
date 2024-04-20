cae683affa152e27e8ce8e4cdc89a26dd45d6f4e
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-20 18:37:54 +0100

    Create FavouritesForm Cmpnt: provision form; implement Toast; handle result state

---
+++ src/components/user/favourites/FavesForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import useToast from "@/components/ui/feedback/toast/useToast";
+import addToFavourties from "@/server/actions/addToFavourties";
+import SubmitButton from "./SubmitButton";
+
+type FavouritesFormProps = {
+  listingId: string;
+  isUserFavourite?: boolean;
+};
+
+const FavouritesForm = (props: FavouritesFormProps) => {
+  const [result, setResult] = useState<string | null>(null);
+
+  const toastTools = useToast();
+  const Toast = toastTools.Toast;
+
+  const handleFormSubmit = async (formData: FormData) => {
+    const response = await addToFavourties(formData);
+    setResult(response);
+    toastTools.handleOpen();
+  };
+
+  const isUserFavourite = !result
+    ? props.isUserFavourite ?? false
+    : result === "Listing added to favourites"
+    ? true
+    : false;
+
+  return (
+    <>
+      <Stack direction="row" alignItems="center">
+        <form action={handleFormSubmit}>
+          <SubmitButton
+            listingId={props.listingId}
+            isUserFavourite={isUserFavourite}
+          />
+        </form>
+
+        <Typography variant="body1" fontWeight={500}>
+          Save
+        </Typography>
+      </Stack>
+
+      {result ? (
+        <Toast
+          open={toastTools.open}
+          message={result}
+          handleClose={toastTools.handleClose}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default FavouritesForm;
