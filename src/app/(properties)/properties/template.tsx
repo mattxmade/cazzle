e63187b7bf382ee7a311b8f805c418f7cc0b7cc3
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-20 15:54:07 +0100

    Remove template.tsx

--- src/app/(properties)/properties/template.tsx
+++
@@ -1,9 +0,0 @@
-import PageTemplate from "@/components/pages/PageTemplate";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function RouteTemplate({ children }: Props) {
-  return <PageTemplate>{children}</PageTemplate>;
-}
