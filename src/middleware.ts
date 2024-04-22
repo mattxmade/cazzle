ad5c004f76d9653e85f50296ea7291756476e609
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-22 19:10:37 +0100

    Create middlewate.ts: implement authMiddleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/not-found",
+    "/sign-in",
+    "/sign-up",
+    "/welcome",
+    "/properties/:path*",
+  ],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
