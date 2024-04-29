cc935770d0325f6dce228de8e437178a82fd46a8
Matt <97047080+mattxmade@users.noreply.github.com>
2024-04-29 18:41:06 +0100

    Expand Component props; implement Mui Card Cmpnt

--- src/components/ui/buttons/ShareButton.tsx
+++ src/components/ui/buttons/ShareButton.tsx
@@ -1,9 +1,36 @@
+import Card, { CardProps } from "@mui/material/Card/Card";
 import Button from "@mui/material/Button";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 
-const ShareButton = () => (
-  <Button>
-    <ShareOutlinedIcon />
+type ShareButtonProps = {
+  elevation?: CardProps["elevation"];
+  color?: React.CSSProperties["color"];
+  borderRadius?: React.CSSProperties["borderRadius"];
+};
+
+const ShareButton = (props: ShareButtonProps) => (
+  <Button
+    type="button"
+    name="share-listing-btn"
+    aria-label="share listing"
+    sx={{
+      minWidth: 0,
+      minHeight: 0,
+      padding: 0.75,
+      borderRadius: props.borderRadius ?? "100%",
+    }}
+  >
+    <Card
+      elevation={props.elevation}
+      sx={{
+        lineHeight: 0,
+        padding: 1,
+        borderRadius: props.borderRadius ?? "100%",
+        color: props.color ?? "inherit",
+      }}
+    >
+      <ShareOutlinedIcon />
+    </Card>
   </Button>
 );
 
